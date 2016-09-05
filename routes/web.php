f519a8db51ec1cecacbfbfaad68f1d2fc7aad545
R. Kukuh <rkukuh@gmail.com>
2016-09-05 20:55:50 +0700

    Create new route

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,16 @@ Route::get('/home', 'HomeController@index');
 
 Route::group(['middleware' => 'auth'], function () {
 
+    /******************************* BASIC ************************************/
+
     Route::get('basic/simple', [
         'as'    => 'basic.simple',
         'uses'  => 'BasicController@simple',
+    ]);
+
+    Route::get('basic/map-coordinates', [
+        'as'    => 'basic.map-coordinates',
+        'uses'  => 'BasicController@mapCoordinates',
     ]);
 
 });
