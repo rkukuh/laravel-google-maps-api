ecb968f4ac81f453a6db976a6bd5abe18f1433d7
R. Kukuh <rkukuh@gmail.com>
2016-09-05 21:15:47 +0700

    Create new view

---
+++ resources/views/basic/language.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('title', 'Localizing the Map')
+
+@section('content')
+    <h1>Localizing the Map</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+<script>
+    // This example displays a map with the language and region set
+    // to Japan. These settings are specified in the HTML script element
+    // when loading the Google Maps JavaScript API.
+    // Setting the language shows the map in the language of your choice.
+    // Setting the region biases the geocoding results to that region.
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 8,
+            center: {lat: 35.717, lng: 139.731}
+        });
+    }
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap&language=ja&region=JP" async defer></script>
+@endpush
