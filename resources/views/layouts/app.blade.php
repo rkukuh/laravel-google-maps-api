da97dd0332384ee3369845fd693cfa0d5a393866
R. Kukuh <rkukuh@gmail.com>
2016-09-06 02:17:44 +0700

    Link a menu to a route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,6 +80,7 @@
                             <li><a href="{{ route('event.closure') }}">Using closures in event listeners</a></li>
                             <li><a href="{{ route('event.argument') }}">Accessing arguments in UI events</a></li>
                             <li><a href="{{ route('event.property') }}">Getting properties with event handlers</a></li>
+                            <li><a href="{{ route('event.dom-listener') }}">Listening to DOM events</a></li>
                         </ul>
                     </li>
                 </ul>
