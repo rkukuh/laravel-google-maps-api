2477ab6c55a76cd14d737a326305ff46fe2da56f
R. Kukuh <rkukuh@gmail.com>
2016-09-05 21:40:38 +0700

    Link a menu to a route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                             <li><a href="{{ route('basic.map-language') }}">Localizing the map</a></li>
                             <li><a href="{{ route('basic.map-rtl') }}">Right to left language</a></li>
                             <li><a href="{{ route('basic.map-sync') }}">Synchronous loading</a></li>
-                            <li><a href="#">Custom map projection</a></li>
+                            <li><a href="{{ route('basic.projection-simple') }}">Custom map projection</a></li>
                         </ul>
                     </li>
                 </ul>
