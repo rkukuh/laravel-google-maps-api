03e79fdaa42a85c3a72d4be6e6e000453bd198f3
R. Kukuh <rkukuh@gmail.com>
2016-09-05 19:41:04 +0700

    Change welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,15 +75,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel + Google Maps API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a>Comining the power of Laravel and the awesomeness of Google Maps API</a>
                 </div>
             </div>
         </div>
