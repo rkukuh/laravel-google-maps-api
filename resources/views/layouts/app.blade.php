2b98ec16f8ad4c941cfb91041177ac6193c1acf7
R. Kukuh <rkukuh@gmail.com>
2016-09-05 23:47:41 +0700

    Reorder attribute

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/app.css">
 
     @stack('css')
 
