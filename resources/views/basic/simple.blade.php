702b8da63131f5dd42f8ac517c38fa9c59a995c9
R. Kukuh <rkukuh@gmail.com>
2016-09-05 20:39:45 +0700

    Add content

--- resources/views/basic/simple.blade.php
+++ resources/views/basic/simple.blade.php
@@ -5,3 +5,24 @@
 
     <div id="map"></div>
 @endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+<script>
+    var map;
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    }
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHnXGlwf8bxR_lc_eAvvkzf7MQYTk6ccE&callback=initMap" async defer></script>
+@endpush
