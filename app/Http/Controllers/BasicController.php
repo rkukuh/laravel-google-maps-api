beed3ae684d9bc2c5831fd3389b04af45eabf510
R. Kukuh <rkukuh@gmail.com>
2016-09-05 20:24:46 +0700

    Create new/required controller

---
+++ app/Http/Controllers/BasicController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class BasicController extends Controller
+{
+    public function simple()
+    {
+        return view('basic.simple');
+    }
+}
