118acd5566dc224a6ff562e72ffa13c7f8eb487e
R. Kukuh <rkukuh@gmail.com>
2016-09-07 23:39:03 +0700

    Create new sub-view

---
+++ resources/views/_navbar/event.blade.php
@@ -0,0 +1,12 @@
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        Events <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+        <li><a href="{{ route('event.simple-click') }}">Simple click event</a></li>
+        <li><a href="{{ route('event.closure') }}">Using closures in event listeners</a></li>
+        <li><a href="{{ route('event.argument') }}">Accessing arguments in UI events</a></li>
+        <li><a href="{{ route('event.property') }}">Getting properties with event handlers</a></li>
+        <li><a href="{{ route('event.dom-listener') }}">Listening to DOM events</a></li>
+    </ul>
+</li>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,18 +49,7 @@
 
                     @include('_navbar.signedin')
 
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            Events <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="{{ route('event.simple-click') }}">Simple click event</a></li>
-                            <li><a href="{{ route('event.closure') }}">Using closures in event listeners</a></li>
-                            <li><a href="{{ route('event.argument') }}">Accessing arguments in UI events</a></li>
-                            <li><a href="{{ route('event.property') }}">Getting properties with event handlers</a></li>
-                            <li><a href="{{ route('event.dom-listener') }}">Listening to DOM events</a></li>
-                        </ul>
-                    </li>
+                    @include('_navbar.event')
                 </ul>
 
                 <!-- Right Side Of Navbar -->
