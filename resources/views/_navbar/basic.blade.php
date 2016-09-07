dc6e4218f52ec289c334ab7fd356bec47f394e1e
R. Kukuh <rkukuh@gmail.com>
2016-09-07 23:36:45 +0700

    Create new sub-view

---
+++ resources/views/_navbar/basic.blade.php
@@ -0,0 +1,14 @@
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        Basic <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+        <li><a href="{{ route('basic.simple') }}">Simple map</a></li>
+        <li><a href="{{ route('basic.map-coordinates') }}">Showing pixel and tile coordinates</a></li>
+        <li><a href="{{ route('basic.map-geolocation') }}">Geolocation</a></li>
+        <li><a href="{{ route('basic.map-language') }}">Localizing the map</a></li>
+        <li><a href="{{ route('basic.map-rtl') }}">Right to left language</a></li>
+        <li><a href="{{ route('basic.map-sync') }}">Synchronous loading</a></li>
+        <li><a href="{{ route('basic.projection-simple') }}">Custom map projection</a></li>
+    </ul>
+</li>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,20 +45,7 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            Basic <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="{{ route('basic.simple') }}">Simple map</a></li>
-                            <li><a href="{{ route('basic.map-coordinates') }}">Showing pixel and tile coordinates</a></li>
-                            <li><a href="{{ route('basic.map-geolocation') }}">Geolocation</a></li>
-                            <li><a href="{{ route('basic.map-language') }}">Localizing the map</a></li>
-                            <li><a href="{{ route('basic.map-rtl') }}">Right to left language</a></li>
-                            <li><a href="{{ route('basic.map-sync') }}">Synchronous loading</a></li>
-                            <li><a href="{{ route('basic.projection-simple') }}">Custom map projection</a></li>
-                        </ul>
-                    </li>
+                    @include('_navbar.basic')
 
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
