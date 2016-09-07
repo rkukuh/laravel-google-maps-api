7b0438335ea049e14df1d6cf8f894e0d6d51f6c7
R. Kukuh <rkukuh@gmail.com>
2016-09-08 01:16:18 +0700

    Create new view

---
+++ resources/views/control/positioning.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('title', 'Control positioning')
+
+@section('content')
+    <h1>Control positioning</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+<script>
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: {lat: -28.643387, lng: 153.612224},
+            mapTypeControl: true,
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+                position: google.maps.ControlPosition.TOP_CENTER
+            },
+            zoomControl: true,
+            zoomControlOptions: {
+                position: google.maps.ControlPosition.LEFT_CENTER
+            },
+            scaleControl: true,
+            streetViewControl: true,
+            streetViewControlOptions: {
+                position: google.maps.ControlPosition.LEFT_TOP
+            },
+            fullscreenControl: true
+        });
+    }
+    </script>
+
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
+@endpush
