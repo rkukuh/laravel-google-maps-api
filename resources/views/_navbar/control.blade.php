c415dc3e99d1dbd9103e5d1e98d0481f97617bb9
R. Kukuh <rkukuh@gmail.com>
2016-09-08 00:31:31 +0700

    Add new navbar menu

--- resources/views/_navbar/control.blade.php
+++ resources/views/_navbar/control.blade.php
@@ -4,5 +4,6 @@
     </a>
     <ul class="dropdown-menu">
         <li><a href="{{ route('control.default') }}">Default controls</a></li>
+        <li><a href="{{ route('control.disable-ui') }}">Disabling default controls</a></li>
     </ul>
 </li>
