6e856368b86278d86a194d71d9aef44e52413ef8
R. Kukuh <rkukuh@gmail.com>
2016-09-15 12:26:14 +0700

    Add divider to navbar menu

--- resources/views/_navbar/drawing.blade.php
+++ resources/views/_navbar/drawing.blade.php
@@ -6,15 +6,20 @@
         <li><a href="{{ route('drawing.simple-marker') }}">Simple marker</a></li>
         <li><a href="{{ route('drawing.marker-label') }}">Marker labels</a></li>
         <li><a href="{{ route('drawing.remove-marker') }}">Remove markers</a></li>
+        <li class="divider"></li>
         <li><a href="{{ route('drawing.simple-marker-icon') }}">Simple marker icon</a></li>
         <li><a href="{{ route('drawing.complex-marker-icon') }}">Complex marker icon</a></li>
+        <li class="divider"></li>
         <li><a href="{{ route('drawing.marker-animation') }}">Marker animation</a></li>
         <li><a href="{{ route('drawing.marker-animation-timeout') }}">Marker animation with setTimeout()</a></li>
+        <li class="divider"></li>
         <li><a href="{{ route('drawing.info-window') }}">Info window</a></li>
         <li><a href="{{ route('drawing.info-window-maxwidth') }}">Info window with maxWidth</a></li>
+        <li class="divider"></li>
         <li><a href="{{ route('drawing.simple-polyline') }}">Simple polylines</a></li>
         <li><a href="{{ route('drawing.removing-polyline') }}">Removing polylines</a></li>
         <li><a href="{{ route('drawing.complex-polyline') }}">Complex polyline</a></li>
         <li><a href="{{ route('drawing.deleting-vertex') }}">Deleting a vertex</a></li>
+        <li class="divider"></li>
     </ul>
 </li>
