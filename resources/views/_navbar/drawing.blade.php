5a9dc1b2791603ffbfe0d51b7a4f5cbb7840db23
R. Kukuh <rkukuh@gmail.com>
2016-09-16 09:40:47 +0700

    Apply multi-menu navbar

--- resources/views/_navbar/drawing.blade.php
+++ resources/views/_navbar/drawing.blade.php
@@ -2,19 +2,36 @@
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
         Drawing <span class="caret"></span>
     </a>
-    <ul class="dropdown-menu">
-        <li><a href="{{ route('drawing.simple-marker') }}">Simple marker</a></li>
-        <li><a href="{{ route('drawing.marker-label') }}">Marker labels</a></li>
-        <li><a href="{{ route('drawing.remove-marker') }}">Remove markers</a></li>
-        <li class="divider"></li>
-        <li><a href="{{ route('drawing.simple-marker-icon') }}">Simple marker icon</a></li>
-        <li><a href="{{ route('drawing.complex-marker-icon') }}">Complex marker icon</a></li>
-        <li class="divider"></li>
-        <li><a href="{{ route('drawing.marker-animation') }}">Marker animation</a></li>
-        <li><a href="{{ route('drawing.marker-animation-timeout') }}">Marker animation with setTimeout()</a></li>
-        <li class="divider"></li>
-        <li><a href="{{ route('drawing.info-window') }}">Info window</a></li>
-        <li><a href="{{ route('drawing.info-window-maxwidth') }}">Info window with maxWidth</a></li>
+    <ul class="dropdown-menu multi-level">
+        <li class="dropdown-submenu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Markers</a>
+            <ul class="dropdown-menu">
+                <li><a href="{{ route('drawing.simple-marker') }}">Simple marker</a></li>
+                <li><a href="{{ route('drawing.marker-label') }}">Marker labels</a></li>
+                <li><a href="{{ route('drawing.remove-marker') }}">Remove markers</a></li>
+            </ul>
+        </li>
+        <li class="dropdown-submenu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Marker icons</a>
+            <ul class="dropdown-menu">
+                <li><a href="{{ route('drawing.simple-marker-icon') }}">Simple marker icon</a></li>
+                <li><a href="{{ route('drawing.complex-marker-icon') }}">Complex marker icon</a></li>
+            </ul>
+        </li>
+        <li class="dropdown-submenu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Marker animations</a>
+            <ul class="dropdown-menu">
+                <li><a href="{{ route('drawing.marker-animation') }}">Marker animation</a></li>
+                <li><a href="{{ route('drawing.marker-animation-timeout') }}">Marker animation with setTimeout()</a></li>
+            </ul>
+        </li>
+        <li class="dropdown-submenu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Info windows</a>
+            <ul class="dropdown-menu">
+                <li><a href="{{ route('drawing.info-window') }}">Info window</a></li>
+                <li><a href="{{ route('drawing.info-window-maxwidth') }}">Info window with maxWidth</a></li>
+            </ul>
+        </li>
         <li class="divider"></li>
         <li><a href="{{ route('drawing.simple-polyline') }}">Simple polylines</a></li>
         <li><a href="{{ route('drawing.removing-polyline') }}">Removing polylines</a></li>
