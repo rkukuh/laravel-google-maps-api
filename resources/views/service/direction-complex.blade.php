45eb9f08bfd12891a0ae88cfdee3ec982cc9ab4e
R. Kukuh <rkukuh@gmail.com>
2016-09-27 18:11:17 +0700

    Create and initialize new view

---
+++ resources/views/service/direction-complex.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Direction (complex)')
+
+@section('content')
+    <h1>Direction (complex)</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+    <script>
+        //
+    </script>
+
+    //
+@endpush
