4939d02460d31eaae3c9e13fc36a8abaea23e959
R. Kukuh <rkukuh@gmail.com>
2016-09-28 18:22:58 +0700

    Create and initialize new view

---
+++ resources/views/service/travel-mode.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Travel Modes in direction')
+
+@section('content')
+    <h1>Travel Modes in direction</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+    <script>
+        //
+    </script>
+
+    //
+@endpush
