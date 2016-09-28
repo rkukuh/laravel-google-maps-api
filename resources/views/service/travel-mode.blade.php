fcf46098c8578e393bb61eaa088fe087f8cafd4c
R. Kukuh <rkukuh@gmail.com>
2016-09-28 18:26:38 +0700

    Add maps' js scripts

--- resources/views/service/travel-mode.blade.php
+++ resources/views/service/travel-mode.blade.php
@@ -6,18 +6,79 @@
     <h1>Travel Modes in direction</h1>
 
     <div id="map"></div>
+
+    <div id="floating-panel">
+        <b>Mode of Travel: </b>
+        <select id="mode">
+            <option value="DRIVING">Driving</option>
+            <option value="WALKING">Walking</option>
+            <option value="BICYCLING">Bicycling</option>
+            <option value="TRANSIT">Transit</option>
+        </select>
+    </div>
 @endsection
 
 @push('css')
     <style>
         #map { height: 500px; }
+
+        #floating-panel {
+            position: absolute;
+            top: 10px;
+            left: 45%;
+            z-index: 5;
+            background-color: #fff;
+            padding: 5px;
+            border: 1px solid #999;
+            text-align: center;
+            font-family: 'Roboto','sans-serif';
+            line-height: 30px;
+            padding-left: 10px;
+        }
     </style>
 @endpush
 
 @push('js')
     <script>
-        //
+        function initMap() {
+            var directionsDisplay = new google.maps.DirectionsRenderer;
+            var directionsService = new google.maps.DirectionsService;
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom    : 14,
+                center  : {lat: 37.77, lng: -122.447}
+            });
+
+            directionsDisplay.setMap(map);
+
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
+
+            document.getElementById('mode').addEventListener('change', function() {
+                calculateAndDisplayRoute(directionsService, directionsDisplay);
+            });
+        }
+
+        function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+            var selectedMode = document.getElementById('mode').value;
+
+            directionsService.route({
+                origin      : {lat: 37.77, lng: -122.447},  // Haight.
+                destination : {lat: 37.768, lng: -122.511},  // Ocean Beach.
+                // Note that Javascript allows us to access the constant
+                // using square brackets and a string value as its
+                // "property."
+                travelMode  : google.maps.TravelMode[selectedMode]
+            },
+            function(response, status) {
+                if (status == 'OK') {
+                    directionsDisplay.setDirections(response);
+                }
+                else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            });
+        }
     </script>
 
-    //
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $server_key }}&callback=initMap"></script>
 @endpush
