5e9ea10bb06a554d25e84d115024a6a910040c94
R. Kukuh <rkukuh@gmail.com>
2016-09-29 18:51:08 +0700

    Add maps' javascripts

--- resources/views/service/waypoint.blade.php
+++ resources/views/service/waypoint.blade.php
@@ -39,7 +39,7 @@
                 <option value="Los Angeles, CA">Los Angeles, CA</option>
             </select>
             <br>
-            
+
             <input type="submit" id="submit">
         </div>
 
@@ -49,7 +49,11 @@
 
 @push('css')
     <style>
-        #map { height: 500px; }
+        #map {
+            float: left;
+            width: 70%;
+            height: 500px;
+        }
 
         #right-panel {
             font-family: 'Roboto','sans-serif';
@@ -88,8 +92,66 @@
 
 @push('js')
     <script>
-        //
+        function initMap() {
+            var directionsService = new google.maps.DirectionsService;
+            var directionsDisplay = new google.maps.DirectionsRenderer;
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom    : 6,
+                center  : {lat: 41.85, lng: -87.65}
+            });
+
+            directionsDisplay.setMap(map);
+
+            document.getElementById('submit').addEventListener('click', function() {
+                calculateAndDisplayRoute(directionsService, directionsDisplay);
+            });
+        }
+
+        function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+            var waypts = [];
+            var checkboxArray = document.getElementById('waypoints');
+
+            for (var i = 0; i < checkboxArray.length; i++) {
+                if (checkboxArray.options[i].selected) {
+                    waypts.push({
+                        location: checkboxArray[i].value,
+                        stopover: true
+                    });
+                }
+            }
+
+            directionsService.route({
+                origin              : document.getElementById('start').value,
+                destination         : document.getElementById('end').value,
+                waypoints           : waypts,
+                optimizeWaypoints   : true,
+                travelMode          : 'DRIVING'
+            },
+            function(response, status) {
+                if (status === 'OK') {
+                    directionsDisplay.setDirections(response);
+
+                    var route = response.routes[0];
+                    var summaryPanel = document.getElementById('directions-panel');
+
+                    summaryPanel.innerHTML = '';
+
+                    // For each route, display summary information.
+                    for (var i = 0; i < route.legs.length; i++) {
+                        var routeSegment = i + 1;
+                        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
+                        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+                        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+                        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+                    }
+                }
+                else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            });
+        }
     </script>
 
-    //
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $server_key }}&callback=initMap"></script>
 @endpush
