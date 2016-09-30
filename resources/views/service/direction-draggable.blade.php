b9786f3b2df5f8a65a5707860951962945cd3b50
R. Kukuh <rkukuh@gmail.com>
2016-09-30 19:16:39 +0700

    Add new html tags and styling them

--- resources/views/service/direction-draggable.blade.php
+++ resources/views/service/direction-draggable.blade.php
@@ -6,11 +6,47 @@
     <h1>Draggable direction</h1>
 
     <div id="map"></div>
+
+    <div id="right-panel">
+        <p>Total Distance: <span id="total"></span></p>
+    </div>
 @endsection
 
 @push('css')
     <style>
-        #map { height: 500px; }
+        #map {
+            float: left;
+            width: 63%;
+            height: 500px;
+        }
+
+        #right-panel {
+            font-family: 'Roboto','sans-serif';
+            line-height: 30px;
+            padding-left: 10px;
+        }
+
+        #right-panel select, #right-panel input {
+            font-size: 15px;
+        }
+
+        #right-panel select {
+            width: 100%;
+        }
+
+        #right-panel i {
+            font-size: 12px;
+        }
+
+        #right-panel {
+            float: right;
+            width: 34%;
+            height: 100%;
+        }
+        .panel {
+            height: 100%;
+            overflow: auto;
+        }
     </style>
 @endpush
 
