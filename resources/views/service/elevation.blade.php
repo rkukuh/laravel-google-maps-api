8ca7400bb37ab574deb4df0b0e9f7dbf44b642af
R. Kukuh <rkukuh@gmail.com>
2016-10-04 22:23:44 +0700

    Add maps' js scripts

--- resources/views/service/elevation.blade.php
+++ resources/views/service/elevation.blade.php
@@ -16,8 +16,42 @@
 
 @push('js')
     <script>
-        //
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom        : 8,
+                center      : {lat: 63.333, lng: -150.5},  // Denali.
+                mapTypeId   : 'terrain'
+            });
+
+            var elevator    = new google.maps.ElevationService;
+            var infowindow  = new google.maps.InfoWindow({map: map});
+
+            map.addListener('click', function(event) {
+                displayLocationElevation(event.latLng, elevator, infowindow);
+            });
+        }
+
+        function displayLocationElevation(location, elevator, infowindow) {
+            elevator.getElevationForLocations({
+                'locations': [location]
+            },
+            function(results, status) {
+                infowindow.setPosition(location);
+
+                if (status === 'OK') {
+                    if (results[0]) {
+                        infowindow.setContent('The elevation at this point <br>is ' + results[0].elevation + ' meters.');
+                    }
+                    else {
+                        infowindow.setContent('No results found');
+                    }
+                }
+                else {
+                    infowindow.setContent('Elevation service failed due to: ' + status);
+                }
+            });
+        }
     </script>
 
-    //
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $server_key }}&callback=initMap"></script>
 @endpush
