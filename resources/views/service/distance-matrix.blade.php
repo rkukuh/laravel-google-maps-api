4bedcf2983561efdd51b8e7b8d2343586283cd08
R. Kukuh <rkukuh@gmail.com>
2016-10-04 21:40:23 +0700

    Create and initialize new view

---
+++ resources/views/service/distance-matrix.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Distance Matrix')
+
+@section('content')
+    <h1>Distance Matrix</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+    <script>
+        //
+    </script>
+
+    //
+@endpush
