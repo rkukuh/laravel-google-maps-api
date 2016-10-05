71825540deb5a81f6b858f61f5d8fcfc0d961bfb
R. Kukuh <rkukuh@gmail.com>
2016-10-05 17:37:06 +0700

    Add maps' js scripts

--- resources/views/service/streetview.blade.php
+++ resources/views/service/streetview.blade.php
@@ -5,19 +5,29 @@
 @section('content')
     <h1>Street View container</h1>
 
-    <div id="map"></div>
+    <div id="street-view"></div>
 @endsection
 
 @push('css')
     <style>
-        #map { height: 500px; }
+        #street-view { height: 500px; }
     </style>
 @endpush
 
 @push('js')
     <script>
-        //
+        var panorama;
+
+        function initialize() {
+            panorama = new google.maps.StreetViewPanorama(
+                document.getElementById('street-view'),
+            {
+                position    : {lat: 37.869260, lng: -122.254811},
+                pov         : {heading: 165, pitch: 0},
+                zoom        : 1
+            });
+        }
     </script>
 
-    //
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initialize"></script>
 @endpush
