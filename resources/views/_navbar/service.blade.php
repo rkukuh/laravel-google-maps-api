6e668d5fd8a7d437443de0ccfd0de5ae8cfcb8e4
R. Kukuh <rkukuh@gmail.com>
2016-10-06 17:34:47 +0700

    Add new menu groups: Direction and Elevation

--- resources/views/_navbar/service.blade.php
+++ resources/views/_navbar/service.blade.php
@@ -12,21 +12,29 @@
                 <li><a href="{{ route('service.geocoding-component-restriction') }}">Geocoding Component Restriction</a></li>
             </ul>
         </li>
+        <li class="dropdown-submenu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Direction</a>
+            <ul class="dropdown-menu">
+                <li><a href="{{ route('service.direction') }}">Direction simple</a></li>
+                <li><a href="{{ route('service.direction-panel') }}">Direction with setPanel()</a></li>
+                <li><a href="{{ route('service.direction-complex') }}">Direction (complex)</a></li>
+                <li><a href="{{ route('service.travel-mode') }}">Travel Mode</a></li>
+                <li><a href="{{ route('service.waypoint') }}">Waypoints</a></li>
+                <li><a href="{{ route('service.direction-draggable') }}">Draggable direction</a></li>
+            </ul>
+        </li>
+        <li class="dropdown-submenu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Elevation</a>
+            <ul class="dropdown-menu">
+                <li><a href="{{ route('service.elevation') }}">Elevation simple</a></li>
+                <li><a href="{{ route('service.elevation-path') }}">Showing elevation along path</a></li>
+            </ul>
+        </li>
 
         <li class="divider"></li>
+        <li><a href="{{ route('service.distance-matrix') }}">Distance Matrix</a></li>
         <li><a href="{{ route('service.region-biasing-es') }}">Region code biasing (ES)</a></li>
         <li><a href="{{ route('service.region-biasing-us') }}">Region code biasing (US)</a></li>
-        <li class="divider"></li>
-        <li><a href="{{ route('service.direction') }}">Direction</a></li>
-        <li><a href="{{ route('service.direction-panel') }}">Direction with setPanel()</a></li>
-        <li><a href="{{ route('service.direction-complex') }}">Direction (complex)</a></li>
-        <li><a href="{{ route('service.travel-mode') }}">Travel Mode</a></li>
-        <li><a href="{{ route('service.waypoint') }}">Waypoints</a></li>
-        <li><a href="{{ route('service.direction-draggable') }}">Draggable direction</a></li>
-        <li class="divider"></li>
-        <li><a href="{{ route('service.distance-matrix') }}">Distance Matrix</a></li>
-        <li><a href="{{ route('service.elevation') }}">Elevation service</a></li>
-        <li><a href="{{ route('service.elevation-path') }}">Showing elevation along path</a></li>
         <li class="divider"></li>
         <li><a href="{{ route('service.streetview') }}">Street View container</a></li>
         <li><a href="{{ route('service.streetview-sidebyside') }}">Street View side-by-side</a></li>
