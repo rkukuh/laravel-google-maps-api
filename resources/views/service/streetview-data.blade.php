53cced5d0e400c77bb08dafce467feadd8292b23
R. Kukuh <rkukuh@gmail.com>
2016-10-06 19:17:18 +0700

    Add maps' js scripts

--- resources/views/service/streetview-data.blade.php
+++ resources/views/service/streetview-data.blade.php
@@ -6,18 +6,83 @@
     <h1>Accessing street view data</h1>
 
     <div id="map"></div>
+    <div id="pano"></div>
 @endsection
 
 @push('css')
     <style>
-        #map { height: 500px; }
+        #map, #pano {
+            height: 500px;
+            width: 50%;
+            float: left;
+        }
     </style>
 @endpush
 
 @push('js')
     <script>
-        //
+        var map;
+        var panorama;
+
+        function initMap() {
+            var berkeley = {lat: 37.869085, lng: -122.254775};
+            var sv       = new google.maps.StreetViewService();
+
+            panorama     = new google.maps.StreetViewPanorama(document.getElementById('pano'));
+
+            // Set up the map.
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: berkeley,
+                zoom: 16,
+                streetViewControl: false
+            });
+
+            // Set the initial Street View camera to the center of the map
+            sv.getPanorama({location: berkeley, radius: 50}, processSVData);
+
+            // Look for a nearby Street View panorama when the map is clicked.
+            // getPanoramaByLocation will return the nearest pano when the
+            // given radius is 50 meters or less.
+            map.addListener('click', function(event) {
+                sv.getPanorama({location: event.latLng, radius: 50}, processSVData);
+            });
+        }
+
+        function processSVData(data, status) {
+            if (status === 'OK') {
+                var marker = new google.maps.Marker({
+                    position: data.location.latLng,
+                    map: map,
+                    title: data.location.description
+                });
+
+                panorama.setPano(data.location.pano);
+
+                panorama.setPov({
+                    heading: 270,
+                    pitch: 0
+                });
+
+                panorama.setVisible(true);
+
+                marker.addListener('click', function() {
+                    var markerPanoID = data.location.pano;
+
+                    // Set the Pano to use the passed panoID.
+                    panorama.setPano(markerPanoID);
+                    panorama.setPov({
+                        heading: 270,
+                        pitch: 0
+                    });
+                    
+                    panorama.setVisible(true);
+                });
+            }
+            else {
+                console.error('Street View data not found for this location.');
+            }
+        }
     </script>
 
-    //
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
