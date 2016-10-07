e8fb7d8500f69e9377206f72e5f7de9878124990
R. Kukuh <rkukuh@gmail.com>
2016-10-07 18:21:56 +0700

    Add maps' js scripts

--- resources/views/service/maximum-zoom.blade.php
+++ resources/views/service/maximum-zoom.blade.php
@@ -16,8 +16,36 @@
 
 @push('js')
     <script>
-        //
+        var map;
+        var maxZoomService;
+        var infoWindow;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 11,
+                center: {lat: 35.6894, lng: 139.692},
+                mapTypeId: 'hybrid'
+            });
+
+            infoWindow      = new google.maps.InfoWindow();
+            maxZoomService  = new google.maps.MaxZoomService();
+
+            map.addListener('click', showMaxZoom);
+        }
+
+        function showMaxZoom(e) {
+            maxZoomService.getMaxZoomAtLatLng(e.latLng, function(response) {
+                if (response.status !== 'OK') {
+                    infoWindow.setContent('Error in MaxZoomService');
+                } else {
+                    infoWindow.setContent('The maximum zoom at this location is: ' + response.zoom);
+                }
+
+                infoWindow.setPosition(e.latLng);
+                infoWindow.open(map);
+            });
+        }
     </script>
 
-    //
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
