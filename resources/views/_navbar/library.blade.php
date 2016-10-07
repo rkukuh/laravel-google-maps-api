d62d7259e14195eaa7fe57cc44e61b0f518debec
R. Kukuh <rkukuh@gmail.com>
2016-10-07 18:28:10 +0700

    Create and initialize new reusable view

---
+++ resources/views/_navbar/library.blade.php
@@ -0,0 +1,8 @@
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        Library <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+        <li><a href="{{ route('library.drawing') }}">Drawing tool</a></li>
+    </ul>
+</li>
