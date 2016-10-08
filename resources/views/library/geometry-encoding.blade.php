2937d6c09f728fa719c97f5f3c8d0c91a33a1a12
R. Kukuh <rkukuh@gmail.com>
2016-10-08 19:20:34 +0700

    Create and initialize new view

---
+++ resources/views/library/geometry-encoding.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Geometry: Encoding methods')
+
+@section('content')
+    <h1>Geometry: Encoding methods</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+    <script>
+        //
+    </script>
+
+    //
+@endpush
