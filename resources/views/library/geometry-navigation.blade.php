b2247f43a5e1abaa3890ea41d3fa2d6f56ed3a2e
R. Kukuh <rkukuh@gmail.com>
2016-10-08 18:40:04 +0700

    Create and initialize new view

---
+++ resources/views/library/geometry-navigation.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Geometry: Navigation functions')
+
+@section('content')
+    <h1>Geometry: Navigation functions</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+    <script>
+        //
+    </script>
+
+    //
+@endpush
