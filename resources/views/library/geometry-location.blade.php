553bc6241cae19c3a0f91e389cc9c7bbdde4fac5
R. Kukuh <rkukuh@gmail.com>
2016-10-09 11:07:16 +0700

    Create an initialize new view

---
+++ resources/views/library/geometry-location.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Geometry: containsLocation()')
+
+@section('content')
+    <h1>Geometry: containsLocation()</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+    <script>
+        //
+    </script>
+
+    //
+@endpush
