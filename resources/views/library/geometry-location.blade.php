fdc2be8e89d5e997cfce13d2419372615a01b0fc
R. Kukuh <rkukuh@gmail.com>
2016-10-09 11:12:54 +0700

    Add maps' js scripts

--- resources/views/library/geometry-location.blade.php
+++ resources/views/library/geometry-location.blade.php
@@ -16,8 +16,46 @@
 
 @push('js')
     <script>
-        //
+        // This example requires the Geometry library. Include the libraries=geometry
+        // parameter when you first load the API. For example:
+        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=geometry">
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 24.886, lng: -70.269},
+                zoom: 5,
+            });
+
+            var triangleCoords = [
+                {lat: 25.774, lng: -80.19},
+                {lat: 18.466, lng: -66.118},
+                {lat: 32.321, lng: -64.757}
+            ];
+
+            var bermudaTriangle = new google.maps.Polygon({paths: triangleCoords});
+
+            google.maps.event.addListener(map, 'click', function(e) {
+                var resultColor =
+                google.maps.geometry.poly.containsLocation(e.latLng, bermudaTriangle) ?
+                'red' :
+                'green';
+
+                new google.maps.Marker({
+                    position: e.latLng,
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        fillColor: resultColor,
+                        fillOpacity: .2,
+                        strokeColor: 'white',
+                        strokeWeight: .5,
+                        scale: 10
+                    }
+                });
+            });
+        }
     </script>
 
-    //
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&libraries=geometry&callback=initMap"></script>
 @endpush
