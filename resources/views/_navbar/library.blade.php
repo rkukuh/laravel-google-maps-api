f1f7b45f1738ebbb71fdcade65dbccbbcb0bd97b
R. Kukuh <rkukuh@gmail.com>
2016-10-10 11:23:30 +0700

    Add new menu

--- resources/views/_navbar/library.blade.php
+++ resources/views/_navbar/library.blade.php
@@ -8,5 +8,7 @@
         <li><a href="{{ route('library.geometry-navigation') }}">Geometry: Navigation function</a></li>
         <li><a href="{{ route('library.geometry-encoding') }}">Geometry: Encoding method</a></li>
         <li><a href="{{ route('library.geometry-location') }}">Geometry: containsLocation()</a></li>
+        <li class="divider"></li>
+        <li><a href="{{ route('library.places-search') }}">Places: Search</a></li>
     </ul>
 </li>
