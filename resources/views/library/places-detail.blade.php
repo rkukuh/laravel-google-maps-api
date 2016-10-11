d5b97af64c04083f693e275ae834afcc36dcf88b
R. Kukuh <rkukuh@gmail.com>
2016-10-11 11:48:13 +0700

    Create and initialize new view

---
+++ resources/views/library/places-detail.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Place detail')
+
+@section('content')
+    <h1>Place detail</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+    <script>
+        //
+    </script>
+
+    //
+@endpush
