90bf911a8d0a9ea32f690b16e0a493d9a2101a0f
R. Kukuh <rkukuh@gmail.com>
2016-10-12 13:03:57 +0700

    Typo

--- resources/views/_navbar/library.blade.php
+++ resources/views/_navbar/library.blade.php
@@ -13,6 +13,6 @@
         <li><a href="{{ route('library.places-detail') }}">Place detail</a></li>
         <li><a href="{{ route('library.places-search-pagination') }}">Place search pagination</a></li>
         <li><a href="{{ route('library.places-radar-search') }}">Search 200 places with Radar Search</a></li>
-        <li><a href="{{ route('library.places-autocomplete') }}">Place: Autocomplete</a></li>
+        <li><a href="{{ route('library.places-autocomplete') }}">Places: Autocomplete</a></li>
     </ul>
 </li>
