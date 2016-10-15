f67ea1f51956f52732e57b817510aceed07c0503
R. Kukuh <rkukuh@gmail.com>
2016-10-15 16:17:25 +0700

    Add maps' js scripts

--- resources/views/library/places-id-finder.blade.php
+++ resources/views/library/places-id-finder.blade.php
@@ -6,18 +6,104 @@
     <h1>PlaceID Finder</h1>
 
     <div id="map"></div>
+
+    <input id="pac-input" class="controls" type="text" placeholder="Enter a location">
 @endsection
 
 @push('css')
     <style>
         #map { height: 500px; }
+
+        .controls {
+            background-color: #fff;
+            border-radius: 2px;
+            border: 1px solid transparent;
+            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+            box-sizing: border-box;
+            font-family: Roboto;
+            font-size: 15px;
+            font-weight: 300;
+            height: 29px;
+            margin-left: 17px;
+            margin-top: 10px;
+            outline: none;
+            padding: 0 11px 0 13px;
+            text-overflow: ellipsis;
+            width: 400px;
+        }
+
+        .controls:focus {
+            border-color: #4d90fe;
+        }
     </style>
 @endpush
 
 @push('js')
     <script>
-        //
+        // This sample uses the Place Autocomplete widget to allow the user to search
+        // for and select a place. The sample then displays an info window containing
+        // the place ID and other information about the place that the user has
+        // selected.
+
+        // This example requires the Places library. Include the libraries=places
+        // parameter when you first load the API. For example:
+        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: -33.8688, lng: 151.2195},
+                zoom: 13
+            });
+
+            var input        = document.getElementById('pac-input');
+            var autocomplete = new google.maps.places.Autocomplete(input);
+
+            autocomplete.bindTo('bounds', map);
+
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+            var infowindow = new google.maps.InfoWindow();
+
+            var marker = new google.maps.Marker({
+                map: map
+            });
+
+            marker.addListener('click', function() {
+                infowindow.open(map, marker);
+            });
+
+            autocomplete.addListener('place_changed', function() {
+                infowindow.close();
+
+                var place = autocomplete.getPlace();
+
+                if ( ! place.geometry) {
+                    return;
+                }
+
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                } else {
+                    map.setCenter(place.geometry.location);
+                    map.setZoom(17);
+                }
+
+                // Set the position of the marker using the place ID and location.
+                marker.setPlace({
+                    placeId: place.place_id,
+                    location: place.geometry.location
+                });
+
+                marker.setVisible(true);
+
+                infowindow.setContent('<strong>' + place.name + '</strong><br>' +
+                                      'Place ID: ' + place.place_id + '<br>' +
+                                      place.formatted_address);
+
+                infowindow.open(map, marker);
+            });
+        }
     </script>
 
-    //
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&libraries=places&callback=initMap"></script>
 @endpush
