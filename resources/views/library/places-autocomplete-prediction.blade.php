8fd829c4708fee14d398e25c0163617c00f1d68f
R. Kukuh <rkukuh@gmail.com>
2016-10-15 15:48:24 +0700

    Create and initialize new view

---
+++ resources/views/library/places-autocomplete-prediction.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Retrieving autocomplete predictions')
+
+@section('content')
+    <h1>Retrieving autocomplete predictions</h1>
+
+    <div id="map"></div>
+@endsection
+
+@push('css')
+    <style>
+        #map { height: 500px; }
+    </style>
+@endpush
+
+@push('js')
+    <script>
+        //
+    </script>
+
+    //
+@endpush
