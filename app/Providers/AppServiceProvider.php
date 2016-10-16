185ea97a710d40765c2235b6497fe4b45ff00312
R. Kukuh <rkukuh@gmail.com>
2016-10-16 09:27:09 +0700

    Add new shared view data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
     {
         View::share('browser_key', 'AIzaSyCHnXGlwf8bxR_lc_eAvvkzf7MQYTk6ccE');
         View::share('server_key', 'AIzaSyByaynCk7vcuS66I4S6Ed46IgreC54UVEg');
+
+        View::share('browser_key_placeholder', 'BROWSER_KEY');
+        View::share('server_key_placeholder', 'SERVER_KEY');
     }
 
     /**
