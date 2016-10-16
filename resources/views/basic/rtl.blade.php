58a96ce5f8c7efd43cf58259e5fa2c2588ce298d
R. Kukuh <rkukuh@gmail.com>
2016-10-16 10:42:00 +0700

    Add/fill source code tab contents

--- resources/views/basic/rtl.blade.php
+++ resources/views/basic/rtl.blade.php
@@ -4,7 +4,11 @@
 @section('title', 'Right-to-Left Languages')
 
 @section('content')
-    <h1>Right-to-Left Languages</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Right-to-Left Languages
+    </h1>
 
     <div id="map"></div>
 @endsection
@@ -17,13 +21,6 @@
 
 @push('js')
     <script>
-        // This example displays a map with the language set to Arabic and the
-        // regions set to Egypt. These settings are specified in the HTML script
-        // element when loading the Google Maps JavaScript API.
-        // Setting the language shows the map in the language of your choice.
-        // Setting the region biases the geocoding results to that region.
-        // In addition, the page's html element sets the text direction to
-        // right-to-left.
         function initMap() {
             var cairo = {lat: 30.064742, lng: 31.249509};
 
@@ -46,3 +43,45 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&language=ar&region=EG&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        // This example displays a map with the language set to Arabic and the regions set to Egypt.
+        // These settings are specified in the HTML script element when loading the Google Maps API.
+        // Setting the language shows the map in the language of your choice.
+        // Setting the region biases the geocoding results to that region.
+        // In addition, the page&apos;s html element sets the text direction to right-to-left.
+
+        function initMap() {
+            var cairo = {lat: 30.064742, lng: 31.249509};
+
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                scaleControl: true,
+                center: cairo,
+                zoom: 10
+            });
+
+            var infowindow = new google.maps.InfoWindow;
+
+            infowindow.setContent(&apos;&lt;b&gt;القاهرة&lt;/b&gt;&apos;);
+
+            var marker = new google.maps.Marker({map: map, position: cairo});
+                marker.addListener(&apos;click&apos;, function() {
+                infowindow.open(map, marker);
+            });
+        }
+    &lt;/script&gt;
+
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;language=ar&amp;region=EG&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
