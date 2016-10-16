7c600ae84ec47da90d7b795dfebcf19935888139
R. Kukuh <rkukuh@gmail.com>
2016-10-16 09:06:24 +0700

    Add/fill section

--- resources/views/basic/simple.blade.php
+++ resources/views/basic/simple.blade.php
@@ -3,9 +3,53 @@
 @section('title', 'Simple Map')
 
 @section('content')
-    <h1>Simple Map</h1>
+    <h1>
+        <button type="button" class="btn btn-primary"
+                data-toggle="modal" data-target="#source_code">
+            <strong>&lt;/&gt;</strong>
+        </button>
+
+        Simple Map
+    </h1>
 
     <div id="map"></div>
+@endsection
+
+@section('source-code-modal')
+    <div class="modal fade" id="source_code">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Source Code</h4>
+                </div>
+                <div class="modal-body">
+                    <div>
+                        <ul class="nav nav-tabs" role="tablist">
+                            <li class="active"><a href="#javascript" data-toggle="tab">Javascript</a></li>
+                            <li><a href="#css" data-toggle="tab">CSS</a></li>
+                            <li><a href="#html" data-toggle="tab">HTML</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="javascript">
+                                javascript
+                            </div>
+                            <div class="tab-pane" id="css">
+                                css
+                            </div>
+                            <div class="tab-pane" id="html">
+                                html
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('css')
