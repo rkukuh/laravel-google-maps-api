3f6405c46465c381eb4703071e6d91ac51bcd890
R. Kukuh <rkukuh@gmail.com>
2016-10-16 10:12:24 +0700

    Add/fill source code tab contents

--- resources/views/basic/geolocation.blade.php
+++ resources/views/basic/geolocation.blade.php
@@ -20,11 +20,6 @@
 
 @push('js')
     <script>
-        // Note: This example requires that you consent to location sharing when
-        // prompted by your browser. If you see the error "The Geolocation service
-        // failed.", it means you probably did not give permission for the browser to
-        // locate you.
-
         function initMap() {
             var map = new google.maps.Map(document.getElementById('map'), {
                 center: {lat: -34.397, lng: 150.644},
@@ -33,7 +28,6 @@
 
             var infoWindow = new google.maps.InfoWindow({map: map});
 
-            // Try HTML5 geolocation.
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function(position) {
                     var pos = {
@@ -50,7 +44,6 @@
                 });
             }
             else {
-                // Browser doesn't support Geolocation
                 handleLocationError(false, infoWindow, map.getCenter());
             }
         }
@@ -68,3 +61,64 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        // Note: This example requires that you consent to location sharing when
+        // prompted by your browser. If you see the error &quot;The Geolocation service
+        // failed.&quot;, it means you probably did not give permission for the browser to
+        // locate you.
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center: {lat: -34.397, lng: 150.644},
+                zoom: 10
+            });
+
+            var infoWindow = new google.maps.InfoWindow({map: map});
+
+            // Try HTML5 geolocation.
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent(&apos;Location found.&apos;);
+
+                    map.setCenter(pos);
+                }, function() {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                });
+            }
+            else {
+                // Browser doesn&apos;t support Geolocation
+                handleLocationError(false, infoWindow, map.getCenter());
+            }
+        }
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+
+            infoWindow.setContent(
+                browserHasGeolocation ?
+                    &apos;Error: The Geolocation service failed.&apos; :
+                    &apos;Error: Your browser doesn\&apos;t support geolocation.&apos;
+            );
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
