b796bad8fbf3ee19f9b0d1291408908e817b6a93
R. Kukuh <rkukuh@gmail.com>
2016-10-16 23:24:10 +0700

    Set center map to Surabaya

--- resources/views/control/options.blade.php
+++ resources/views/control/options.blade.php
@@ -15,21 +15,20 @@
 @endpush
 
 @push('js')
-<div id="map"></div>
-<script>
-    // You can set control options to change the default position or style of many
-    // of the map controls.
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 4,
-            center: {lat: -33, lng: 151},
-            mapTypeControl: true,
-            mapTypeControlOptions: {
-                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-                mapTypeIds: ['roadmap', 'terrain']
-            }
-        });
-    }
+    <script>
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: -7.265757, lng: 112.734146},
+                zoom: 7,
+                mapTypeControl: true,
+                mapTypeControlOptions: {
+                    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+                    mapTypeIds: [
+                        google.maps.MapTypeId.ROADMAP,
+                        google.maps.MapTypeId.SATELLITE]
+                }
+            });
+        }
     </script>
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
