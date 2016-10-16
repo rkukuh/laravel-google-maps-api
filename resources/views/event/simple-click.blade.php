f815c3f2f3d994ec194c727e2ecae08e46765a91
R. Kukuh <rkukuh@gmail.com>
2016-10-16 11:28:21 +0700

    Add/fill source code tab contents

--- resources/views/event/simple-click.blade.php
+++ resources/views/event/simple-click.blade.php
@@ -35,7 +35,6 @@
             });
 
             map.addListener('center_changed', function() {
-                // 3 seconds after the center of the map has changed, pan back to the marker.
                 window.setTimeout(function() {
                     map.panTo(marker.getPosition());
                 }, 3000);
@@ -50,3 +49,46 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var myLatlng = {lat: -7.265757, lng: 112.734146};
+
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom: 8,
+                center: myLatlng
+            });
+
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: &apos;Click to zoom&apos;
+            });
+
+            map.addListener(&apos;center_changed&apos;, function() {
+                // 3 seconds after the center of the map has changed, pan back to the marker.
+                window.setTimeout(function() {
+                    map.panTo(marker.getPosition());
+                }, 3000);
+            });
+
+            marker.addListener(&apos;click&apos;, function() {
+                map.setZoom(8);
+                map.setCenter(marker.getPosition());
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
