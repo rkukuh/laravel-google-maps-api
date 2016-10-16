0d3e3ec631aaca9323681e178365e3f61bd89391
R. Kukuh <rkukuh@gmail.com>
2016-10-16 10:31:41 +0700

    Add/fill source code tab contents

--- resources/views/basic/language.blade.php
+++ resources/views/basic/language.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Localizing the Map')
 
 @section('content')
-    <h1>Localizing the Map <small>(ID to JA)</small></h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Localizing the Map <small>(ID to JA)</small>
+    </h1>
 
     <div id="map"></div>
 @endsection
@@ -16,11 +20,6 @@
 
 @push('js')
     <script>
-        // This example displays a map with the language and region set
-        // to Japan. These settings are specified in the HTML script element
-        // when loading the Google Maps JavaScript API.
-        // Setting the language shows the map in the language of your choice.
-        // Setting the region biases the geocoding results to that region.
         function initMap() {
             var map = new google.maps.Map(document.getElementById('map'), {
                 center: {lat: -7.265757, lng: 112.734146},
@@ -29,5 +28,34 @@
         }
     </script>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap&language=ja&region=JP" async defer></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap&language=ja&region=JP"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        // This example displays a map with the language and region set
+        // to Japan. These settings are specified in the HTML script element
+        // when loading the Google Maps JavaScript API.
+        // Setting the language shows the map in the language of your choice.
+        // Setting the region biases the geocoding results to that region.
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center: {lat: -7.265757, lng: 112.734146},
+                zoom: 8,
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&amp;language=ja&amp;region=JP&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
