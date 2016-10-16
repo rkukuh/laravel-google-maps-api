8fc440988d70d24119c60a37c74e02efafe97a3c
R. Kukuh <rkukuh@gmail.com>
2016-10-16 23:25:24 +0700

    Add/fill source code tab contents

--- resources/views/control/options.blade.php
+++ resources/views/control/options.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Control options')
 
 @section('content')
-    <h1>Control options</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Control options
+    </h1>
 
     <div id="map"></div>
 @endsection
@@ -33,3 +37,33 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center: {lat: -7.265757, lng: 112.734146},
+                zoom: 7,
+                mapTypeControl: true,
+                mapTypeControlOptions: {
+                    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+                    mapTypeIds: [
+                        google.maps.MapTypeId.ROADMAP,
+                        google.maps.MapTypeId.SATELLITE]
+                }
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
