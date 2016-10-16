598400cf0da6d795fa463fb237b1ab98e56c0b69
R. Kukuh <rkukuh@gmail.com>
2016-10-16 23:16:52 +0700

    Add/fill source code tab contents

--- resources/views/control/disable-ui.blade.php
+++ resources/views/control/disable-ui.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Disabling default controls')
 
 @section('content')
-    <h1>Disabling default controls</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Disabling default controls
+    </h1>
 
     <div id="map"></div>
 @endsection
@@ -16,14 +20,38 @@
 
 @push('js')
     <script>
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -7.265757, lng: 112.734146},
-            zoom: 7,
-            disableDefaultUI: true
-        });
-    }
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: -7.265757, lng: 112.734146},
+                zoom: 7,
+                disableDefaultUI: true
+            });
+        }
     </script>
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center: {lat: -7.265757, lng: 112.734146},
+                zoom: 7,
+                disableDefaultUI: true
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
