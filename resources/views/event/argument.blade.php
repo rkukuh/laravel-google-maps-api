7ab1f013dbfe849d6347a3bc4089d385f4696024
R. Kukuh <rkukuh@gmail.com>
2016-10-16 12:02:15 +0700

    Add/fill source code tab contents

--- resources/views/event/argument.blade.php
+++ resources/views/event/argument.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Accessing arguments in UI events')
 
 @section('content')
-    <h1>Accessing arguments in UI events</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Accessing arguments in UI events
+    </h1>
 
     <div id="map"></div>
 @endsection
@@ -39,3 +43,39 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center: {lat: -7.265757, lng: 112.734146},
+                zoom: 8
+            });
+
+            map.addListener(&apos;click&apos;, function(e) {
+                placeMarkerAndPanTo(e.latLng, map);
+            });
+        }
+
+        function placeMarkerAndPanTo(latLng, map) {
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+            });
+
+            map.panTo(latLng);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
