c1ef04eff76d7aa84b30520217836a040527e017
R. Kukuh <rkukuh@gmail.com>
2016-10-16 23:09:38 +0700

    Set center map to Surabaya

--- resources/views/control/default.blade.php
+++ resources/views/control/default.blade.php
@@ -18,8 +18,8 @@
     <script>
     function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
+            center: {lat: -7.265757, lng: 112.734146},
+            zoom: 10
         });
     }
     </script>
