43ca6b20a0077e5984468e570750e1f8b116815e
R. Kukuh <rkukuh@gmail.com>
2016-10-16 10:02:37 +0700

    Add/fill source code tab contents

--- resources/views/basic/coordinates.blade.php
+++ resources/views/basic/coordinates.blade.php
@@ -67,13 +67,9 @@
             ].join('<br>');
         }
 
-        // The mapping between latitude, longitude and pixels is defined by the web
-        // mercator projection.
         function project(latLng) {
             var siny = Math.sin(latLng.lat() * Math.PI / 180);
 
-            // Truncating to 0.9999 effectively limits latitude to 89.189. This is
-            // about a third of a tile past the edge of the world tile.
             siny = Math.min(Math.max(siny, -0.9999), 0.9999);
 
             return new google.maps.Point(
@@ -88,7 +84,72 @@
 
 @section('source-code-javascript')
 
-    //
+    &lt;script&gt;
+        function initMap() {
+            var surabaya = new google.maps.LatLng(-7.265757, 112.734146);
+
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center: surabaya,
+                zoom: 9
+            });
+
+            var coordInfoWindow = new google.maps.InfoWindow();
+
+            coordInfoWindow.setContent(createInfoWindowContent(surabaya, map.getZoom()));
+            coordInfoWindow.setPosition(surabaya);
+            coordInfoWindow.open(map);
+
+            map.addListener(&apos;zoom_changed&apos;, function() {
+                coordInfoWindow.setContent(createInfoWindowContent(surabaya, map.getZoom()));
+                coordInfoWindow.open(map);
+            });
+        }
+
+        var TILE_SIZE = 256;
+
+        function createInfoWindowContent(latLng, zoom) {
+            var scale = 1 &lt;&lt; zoom;
+
+            var worldCoordinate = project(latLng);
+
+            var pixelCoordinate = new google.maps.Point(
+                Math.floor(worldCoordinate.x * scale),
+                Math.floor(worldCoordinate.y * scale)
+            );
+
+            var tileCoordinate = new google.maps.Point(
+                Math.floor(worldCoordinate.x * scale / TILE_SIZE),
+                Math.floor(worldCoordinate.y * scale / TILE_SIZE)
+            );
+
+            return [
+                &apos;&lt;b&gt;Surabaya, East Java, Indonesia&lt;/b&gt;&apos;,
+                &apos;LatLng: &apos; + latLng,
+                &apos;Zoom level: &apos; + zoom,
+                &apos;World Coordinate: &apos; + worldCoordinate,
+                &apos;Pixel Coordinate: &apos; + pixelCoordinate,
+                &apos;Tile Coordinate: &apos; + tileCoordinate
+            ].join(&apos;&lt;br&gt;&apos;);
+        }
+
+        // The mapping between latitude, longitude and pixels is defined by the web
+        // mercator projection.
+        function project(latLng) {
+            var siny = Math.sin(latLng.lat() * Math.PI / 180);
+
+            // Truncating to 0.9999 effectively limits latitude to 89.189. This is
+            // about a third of a tile past the edge of the world tile.
+            siny = Math.min(Math.max(siny, -0.9999), 0.9999);
+
+            return new google.maps.Point(
+                TILE_SIZE * (0.5 + latLng.lng() / 360),
+                TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI))
+            );
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
 @endsection
 
 @section('source-code-css')
