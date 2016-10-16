3959e7e7546d7a5c14031c7fec0b366bdc32f7e9
R. Kukuh <rkukuh@gmail.com>
2016-10-16 23:57:06 +0700

    Remove unused comments

--- resources/views/control/state.blade.php
+++ resources/views/control/state.blade.php
@@ -45,85 +45,53 @@
         var map;
         var surabaya = {lat: -7.265757, lng: 112.734146};
 
-        /**
-        * The CenterControl adds a control to the map that recenters the map on surabaya.
-        * @constructor
-        * @param {!Element} controlDiv
-        * @param {!google.maps.Map} map
-        * @param {?google.maps.LatLng} center
-        */
         function CenterControl(controlDiv, map, center) {
-            // We set up a variable for this since we're adding event listeners later.
             var control = this;
 
-            // Set the center property upon construction
             control.center_ = center;
             controlDiv.style.clear = 'both';
 
-            // Set CSS for the control border
             var goCenterUI      = document.createElement('div');
             goCenterUI.id       = 'goCenterUI';
             goCenterUI.title    = 'Click to recenter the map';
 
             controlDiv.appendChild(goCenterUI);
 
-            // Set CSS for the control interior
             var goCenterText        = document.createElement('div');
             goCenterText.id         = 'goCenterText';
             goCenterText.innerHTML  = 'Center Map';
 
             goCenterUI.appendChild(goCenterText);
 
-            // Set CSS for the setCenter control border
             var setCenterUI     = document.createElement('div');
             setCenterUI.id      = 'setCenterUI';
             setCenterUI.title   = 'Click to change the center of the map';
 
             controlDiv.appendChild(setCenterUI);
 
-            // Set CSS for the control interior
             var setCenterText       = document.createElement('div');
             setCenterText.id        = 'setCenterText';
             setCenterText.innerHTML = 'Set Center';
 
             setCenterUI.appendChild(setCenterText);
 
-            // Set up the click event listener for 'Center Map': Set the center of
-            // the map
-            // to the current center of the control.
             goCenterUI.addEventListener('click', function() {
                 var currentCenter = control.getCenter();
-
                 map.setCenter(currentCenter);
             });
 
-            // Set up the click event listener for 'Set Center': Set the center of
-            // the control to the current center of the map.
             setCenterUI.addEventListener('click', function() {
                 var newCenter = map.getCenter();
-
                 control.setCenter(newCenter);
             });
         }
 
-        /**
-        * Define a property to hold the center state.
-        * @private
-        */
         CenterControl.prototype.center_ = null;
 
-        /**
-        * Gets the map center.
-        * @return {?google.maps.LatLng}
-        */
         CenterControl.prototype.getCenter = function() {
             return this.center_;
         };
 
-        /**
-        * Sets the map center.
-        * @param {?google.maps.LatLng} center
-        */
         CenterControl.prototype.setCenter = function(center) {
             this.center_ = center;
         };
@@ -134,10 +102,8 @@
                 center: surabaya
             });
 
-            // Create the DIV to hold the control and call the CenterControl()
-            // constructor passing in this DIV.
-            var centerControlDiv    = document.createElement('div');
-            var centerControl       = new CenterControl(centerControlDiv, map, surabaya);
+            var centerControlDiv = document.createElement('div');
+            var centerControl = new CenterControl(centerControlDiv, map, surabaya);
 
             centerControlDiv.index = 1;
             centerControlDiv.style['padding-top'] = '10px';
