48ae14ff82b303697bb8797079c14a032ad14fb0
R. Kukuh <rkukuh@gmail.com>
2016-11-07 16:33:48 +0700

    Set center map to Surabaya

--- resources/views/style/map-type.blade.php
+++ resources/views/style/map-type.blade.php
@@ -45,8 +45,8 @@
             // Create a map object, and include the MapTypeId to add
             // to the map type control.
             var map = new google.maps.Map(document.getElementById('map'), {
-                    center: {lat: 55.647, lng: 37.581},
-                    zoom: 11,
+                    center: {lat: -7.265757, lng: 112.734146},
+                    zoom: 15,
                     mapTypeControlOptions: {
                     mapTypeIds: [
                         'roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map'
