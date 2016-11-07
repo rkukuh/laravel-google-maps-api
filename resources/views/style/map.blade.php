0abb2a4e6b736a01ee756544484e514bc4b3a3d0
R. Kukuh <rkukuh@gmail.com>
2016-11-07 16:21:11 +0700

    Add/fill source code tab contents

--- resources/views/style/map.blade.php
+++ resources/views/style/map.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Styled maps')
 
 @section('content')
-    <h1>Styled maps</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Styled maps
+    </h1>
 
     <div id="map"></div>
 @endsection
@@ -18,7 +22,7 @@
     <script>
         function initMap() {
             var map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 40.674, lng: -73.945},
+                center: {lat: -7.265757, lng: 112.734146},
                 zoom: 12,
                 styles: [
                     {
@@ -46,3 +50,47 @@
         </script>
         <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+        &lt;script&gt;
+            function initMap() {
+                var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                    center: {lat: -7.265757, lng: 112.734146},
+                    zoom: 12,
+                    styles: [
+                        {
+                            featureType: &apos;all&apos;,
+                            stylers: [
+                                { saturation: -80 }
+                            ]
+                        }, {
+                            featureType: &apos;road.arterial&apos;,
+                            elementType: &apos;geometry&apos;,
+                            stylers: [
+                                { hue: &apos;#00ffee&apos; },
+                                { saturation: 50 }
+                            ]
+                        }, {
+                            featureType: &apos;poi.business&apos;,
+                            elementType: &apos;labels&apos;,
+                            stylers: [
+                                { visibility: &apos;off&apos; }
+                            ]
+                        }
+                    ]
+                });
+            }
+        &lt;/script&gt;
+
+        &lt;script async defer
+            src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
