fdf3adebf2195b98a0eb5431cc0fa27122577008
R. Kukuh <rkukuh@gmail.com>
2016-11-08 18:09:44 +0700

    Remove unused comments

--- resources/views/drawing/marker-label.blade.php
+++ resources/views/drawing/marker-label.blade.php
@@ -22,8 +22,6 @@
     <script src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}"></script>
 
     <script>
-        // In the following example, markers appear when the user clicks on the map.
-        // Each marker is labeled with a single alphabetical character.
         var labels      = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         var labelIndex  = 0;
 
@@ -35,19 +33,14 @@
                 center  : bangalore
             });
 
-            // This event listener calls addMarker() when the map is clicked.
             google.maps.event.addListener(map, 'click', function(event) {
                 addMarker(event.latLng, map);
             });
 
-            // Add a marker at the center of the map.
             addMarker(bangalore, map);
         }
 
-        // Adds a marker to the map.
         function addMarker(location, map) {
-            // Add the marker at the clicked location, and add the next-available label
-            // from the array of alphabetical characters.
             var marker = new google.maps.Marker({
                 position    : location,
                 label       : labels[labelIndex++ % labels.length],
