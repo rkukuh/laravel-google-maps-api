f9924b978b2fe649995e496332971b40663c87a2
R. Kukuh <rkukuh@gmail.com>
2016-11-10 00:14:03 +0700

    Remove unused comment

--- resources/views/drawing/info-window-maxwidth.blade.php
+++ resources/views/drawing/info-window-maxwidth.blade.php
@@ -20,10 +20,6 @@
 
 @push('js')
     <script>
-        // This example displays a marker at the center of Australia.
-        // When the user clicks the marker, an info window opens.
-        // The maximum width of the info window is set to 200 pixels.
-
         function initMap() {
             var uluru = {lat: -25.363, lng: 131.044};
 
