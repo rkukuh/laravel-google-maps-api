c443af8a2f8c3f42ff310466411b793eaa621377
R. Kukuh <rkukuh@gmail.com>
2016-11-09 23:05:28 +0700

    Fill source code into its tabs

--- resources/views/drawing/custom-marker-icon.blade.php
+++ resources/views/drawing/custom-marker-icon.blade.php
@@ -45,3 +45,41 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 8,
+                center  : {lat: -7.265757, lng: 112.734146}
+            });
+
+            var goldStar = {
+                path        : &apos;M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z&apos;,
+                fillColor   : &apos;yellow&apos;,
+                fillOpacity : 0.8,
+                scale       : 1,
+                strokeColor : &apos;gold&apos;,
+                strokeWeight: 10
+            };
+
+            var marker = new google.maps.Marker({
+                position    : map.getCenter(),
+                icon        : goldStar,
+                map         : map
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
