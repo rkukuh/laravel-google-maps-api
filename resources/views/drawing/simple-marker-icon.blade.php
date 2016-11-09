a725033868857b376d87f62b1509f0b3da31a1a4
R. Kukuh <rkukuh@gmail.com>
2016-11-09 22:58:10 +0700

    Fill source code into its tabs

--- resources/views/drawing/simple-marker-icon.blade.php
+++ resources/views/drawing/simple-marker-icon.blade.php
@@ -38,3 +38,34 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 6,
+                center  : {lat: -7.265757, lng: 112.734146}
+            });
+
+            var image = &apos;https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png&apos;;
+
+            var beachMarker = new google.maps.Marker({
+                position    : {lat: -7.265757, lng: 112.734146},
+                map         : map,
+                icon        : image
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
