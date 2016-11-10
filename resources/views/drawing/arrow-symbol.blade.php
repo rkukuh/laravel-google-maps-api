595302770daf2eb86435e042249efc9c32455c42
R. Kukuh <rkukuh@gmail.com>
2016-11-10 23:39:14 +0700

    Add 'show source code' button

--- resources/views/drawing/arrow-symbol.blade.php
+++ resources/views/drawing/arrow-symbol.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Arrow symbol')
 
 @section('content')
-    <h1>Arrow symbol</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Arrow symbol
+    </h1>
 
     <div id="map"></div>
 @endsection
