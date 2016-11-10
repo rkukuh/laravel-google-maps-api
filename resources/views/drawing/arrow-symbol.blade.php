5f9a38c361b27e2e392a966dc81816791f29a83a
R. Kukuh <rkukuh@gmail.com>
2016-11-10 23:42:15 +0700

    Fill source code into its tabs

--- resources/views/drawing/arrow-symbol.blade.php
+++ resources/views/drawing/arrow-symbol.blade.php
@@ -47,3 +47,43 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom        : 15,
+                center      : {lat: -7.265757, lng: 112.734146},
+                mapTypeId   : &apos;terrain&apos;
+            });
+
+            var lineSymbol = {
+                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
+            };
+
+            var line = new google.maps.Polyline({
+                path: [
+                    {lat: -7.265757, lng: 112.734146},
+                    {lat: -7.257757, lng: 112.734146},
+                ],
+                icons: [{
+                    icon: lineSymbol,
+                    offset: &apos;100%&apos;
+                }],
+                map: map
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
