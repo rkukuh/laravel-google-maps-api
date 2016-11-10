8a4dce8d3750c08ba73e9d8f43758468b7c905ab
R. Kukuh <rkukuh@gmail.com>
2016-11-10 23:36:09 +0700

    Fill source code into its tabs

--- resources/views/drawing/predefined-marker.blade.php
+++ resources/views/drawing/predefined-marker.blade.php
@@ -40,3 +40,36 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 10,
+                center  : {lat: -7.265757, lng: 112.734146}
+            });
+
+            var marker = new google.maps.Marker({
+                position: map.getCenter(),
+                icon: {
+                    path: google.maps.SymbolPath.CIRCLE,
+                    scale: 30
+                },
+                draggable: true,
+                map: map
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
