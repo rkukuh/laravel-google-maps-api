3e913689d092957cac26ed282a345825441994b1
R. Kukuh <rkukuh@gmail.com>
2016-11-10 23:49:17 +0700

    Fill source code into its tabs

--- resources/views/drawing/dashed-line.blade.php
+++ resources/views/drawing/dashed-line.blade.php
@@ -52,3 +52,48 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom        : 6,
+                center      : {lat: 20.291, lng: 153.027},
+                mapTypeId   : &apos;terrain&apos;
+            });
+
+            var lineSymbol = {
+                path: &apos;M 0,-1 0,1&apos;,
+                strokeOpacity: 1,
+                scale: 4
+            };
+
+            // Repeat the symbol at intervals of 20 pixels to create the dashed effect.
+            var line = new google.maps.Polyline({
+                path: [
+                    {lat: 22.291, lng: 153.027},
+                    {lat: 18.291, lng: 153.027}
+                ],
+                strokeOpacity: 0,
+                icons: [{
+                    icon: lineSymbol,
+                    offset: &apos;0&apos;,
+                    repeat: &apos;20px&apos;
+                }],
+                map: map
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
