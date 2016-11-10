eaabb210e02c854c24cb185c3e5e681535df82bf
R. Kukuh <rkukuh@gmail.com>
2016-11-10 23:52:29 +0700

    Fill source code into its tabs

--- resources/views/drawing/custom-symbol.blade.php
+++ resources/views/drawing/custom-symbol.blade.php
@@ -70,3 +70,66 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom        : 6,
+                center      : {lat: 20.291, lng: 153.027},
+                mapTypeId   : &apos;terrain&apos;
+            });
+
+            var symbolOne = {
+                path: &apos;M -2,0 0,-2 2,0 0,2 z&apos;,
+                strokeColor: &apos;#F00&apos;,
+                fillColor: &apos;#F00&apos;,
+                fillOpacity: 1
+            };
+
+            var symbolTwo = {
+                path: &apos;M -1,0 A 1,1 0 0 0 -3,0 1,1 0 0 0 -1,0M 1,0 A 1,1 0 0 0 3,0 1,1 0 0 0 1,0M -3,3 Q 0,5 3,3&apos;,
+                strokeColor: &apos;#00F&apos;,
+                rotation: 45
+            };
+
+            var symbolThree = {
+                path: &apos;M -2,-2 2,2 M 2,-2 -2,2&apos;,
+                strokeColor: &apos;#292&apos;,
+                strokeWeight: 4
+            };
+
+            var line = new google.maps.Polyline({
+                path: [
+                    {lat: 22.291, lng: 153.027},
+                    {lat: 18.291, lng: 153.027}
+                ],
+                icons: [
+                    {
+                        icon: symbolOne,
+                        offset: &apos;0%&apos;
+                    }, {
+                        icon: symbolTwo,
+                        offset: &apos;50%&apos;
+                    }, {
+                        icon: symbolThree,
+                        offset: &apos;100%&apos;
+                    }
+                ],
+                map: map
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
