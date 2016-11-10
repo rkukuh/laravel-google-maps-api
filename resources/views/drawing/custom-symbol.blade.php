ab23734bea0e0e4ad4409a72704a05ea55995e2b
R. Kukuh <rkukuh@gmail.com>
2016-11-10 23:51:24 +0700

    Add 'show source code' button

--- resources/views/drawing/custom-symbol.blade.php
+++ resources/views/drawing/custom-symbol.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Custom symbols')
 
 @section('content')
-    <h1>Custom symbols</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Custom symbols
+    </h1>
 
     <div id="map"></div>
 @endsection
