baf206337daafd5285e6b0adda649b4bb9a4638d
R. Kukuh <rkukuh@gmail.com>
2016-11-11 22:56:07 +0700

    Add 'show source code' button

--- resources/views/drawing/simple-polygon.blade.php
+++ resources/views/drawing/simple-polygon.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Simple polygons')
 
 @section('content')
-    <h1>Simple polygons</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Simple polygons
+    </h1>
 
     <div id="map"></div>
 @endsection
