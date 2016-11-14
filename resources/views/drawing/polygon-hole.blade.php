3409e66e2defe7193ee9ccbdba831ac640698087
R. Kukuh <rkukuh@gmail.com>
2016-11-15 00:14:59 +0700

    Remove unused comment

--- resources/views/drawing/polygon-hole.blade.php
+++ resources/views/drawing/polygon-hole.blade.php
@@ -26,23 +26,18 @@
                 center  : {lat: 24.886, lng: -70.268},
             });
 
-            // Define the LatLng coordinates for the polygon's  outer path.
             var outerCoords = [
                 {lat: 25.774, lng: -80.190},
                 {lat: 18.466, lng: -66.118},
                 {lat: 32.321, lng: -64.757}
             ];
 
-            // Define the LatLng coordinates for the polygon's inner path.
-            // Note that the points forming the inner path are wound in the
-            // opposite direction to those in the outer path, to form the hole.
             var innerCoords = [
                 {lat: 28.745, lng: -70.579},
                 {lat: 29.570, lng: -67.514},
                 {lat: 27.339, lng: -66.668}
             ];
 
-            // Construct the polygon, including both paths.
             var bermudaTriangle = new google.maps.Polygon({
                 paths           : [outerCoords, innerCoords],
                 strokeColor     : '#FFC107',
@@ -58,7 +53,6 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
-
 
 @section('source-code-javascript')
 
