210594089ecf54de1fd3bd74c4b4d53d8fba01b5
R. Kukuh <rkukuh@gmail.com>
2016-11-15 00:20:29 +0700

    Spacing

--- resources/views/drawing/polygon-draggable.blade.php
+++ resources/views/drawing/polygon-draggable.blade.php
@@ -24,49 +24,49 @@
 
 @push('js')
     <script>
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom        : 1,
-            center      : {lat: 24.886, lng: -70.268},
-            mapTypeId   : 'terrain'
-        });
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom        : 1,
+                center      : {lat: 24.886, lng: -70.268},
+                mapTypeId   : 'terrain'
+            });
 
-        var blueCoords = [
-            {lat: 25.774, lng: -60.190},
-            {lat: 18.466, lng: -46.118},
-            {lat: 32.321, lng: -44.757}
-        ];
+            var blueCoords = [
+                {lat: 25.774, lng: -60.190},
+                {lat: 18.466, lng: -46.118},
+                {lat: 32.321, lng: -44.757}
+            ];
 
-        var redCoords = [
-            {lat: 25.774, lng: -80.190},
-            {lat: 18.466, lng: -66.118},
-            {lat: 32.321, lng: -64.757}
-        ];
+            var redCoords = [
+                {lat: 25.774, lng: -80.190},
+                {lat: 18.466, lng: -66.118},
+                {lat: 32.321, lng: -64.757}
+            ];
 
-        new google.maps.Polygon({
-            map             : map,
-            paths           : redCoords,
-            strokeColor     : '#FF0000',
-            strokeOpacity   : 0.8,
-            strokeWeight    : 2,
-            fillColor       : '#FF0000',
-            fillOpacity     : 0.35,
-            draggable       : true,
-            geodesic        : true
-        });
+            new google.maps.Polygon({
+                map             : map,
+                paths           : redCoords,
+                strokeColor     : '#FF0000',
+                strokeOpacity   : 0.8,
+                strokeWeight    : 2,
+                fillColor       : '#FF0000',
+                fillOpacity     : 0.35,
+                draggable       : true,
+                geodesic        : true
+            });
 
-        new google.maps.Polygon({
-            map             : map,
-            paths           : blueCoords,
-            strokeColor     : '#0000FF',
-            strokeOpacity   : 0.8,
-            strokeWeight    : 2,
-            fillColor       : '#0000FF',
-            fillOpacity     : 0.35,
-            draggable       : true,
-            geodesic        : false
-        });
-    }
+            new google.maps.Polygon({
+                map             : map,
+                paths           : blueCoords,
+                strokeColor     : '#0000FF',
+                strokeOpacity   : 0.8,
+                strokeWeight    : 2,
+                fillColor       : '#0000FF',
+                fillOpacity     : 0.35,
+                draggable       : true,
+                geodesic        : false
+            });
+        }
     </script>
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
