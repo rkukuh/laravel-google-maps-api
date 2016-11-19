cc240a1cb8e6269f8896ee3f8229ac09e7ca16af
R. Kukuh <rkukuh@gmail.com>
2016-11-19 15:01:45 +0700

    Fill source code tabs

--- resources/views/drawing/rectangle-zoom.blade.php
+++ resources/views/drawing/rectangle-zoom.blade.php
@@ -48,3 +48,42 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom        : 11,
+                center      : {lat: 40.748520, lng: -73.981687},
+                mapTypeId   : &apos;terrain&apos;
+            });
+
+            var rectangle = new google.maps.Rectangle();
+
+            map.addListener(&apos;zoom_changed&apos;, function() {
+                // Get the current bounds, which reflect the bounds before the zoom.
+                rectangle.setOptions({
+                    strokeColor     : &apos;#FF0000&apos;,
+                    strokeOpacity   : 0.8,
+                    strokeWeight    : 2,
+                    fillColor       : &apos;#FF0000&apos;,
+                    fillOpacity     : 0.35,
+                    map             : map,
+                    bounds          : map.getBounds()
+                });
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
