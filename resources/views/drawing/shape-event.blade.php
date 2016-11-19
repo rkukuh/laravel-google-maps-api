ff652f23b8a56b7ba4bb597aa29cade4cc491080
R. Kukuh <rkukuh@gmail.com>
2016-11-19 15:31:14 +0700

    Fill source code tabs

--- resources/views/drawing/shape-event.blade.php
+++ resources/views/drawing/shape-event.blade.php
@@ -69,3 +69,61 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        var rectangle;
+        var map;
+        var infoWindow;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 9,
+                center  : {lat: 44.5452, lng: -78.5389}
+            });
+
+            var bounds = {
+                north: 44.599,
+                south: 44.490,
+                east: -78.443,
+                west: -78.649
+            };
+
+            rectangle = new google.maps.Rectangle({
+                bounds: bounds,
+                editable: true,
+                draggable: true
+            });
+
+            rectangle.setMap(map);
+            rectangle.addListener(&apos;bounds_changed&apos;, showNewRect);
+
+            infoWindow = new google.maps.InfoWindow();
+        }
+
+        function showNewRect(event) {
+            var ne = rectangle.getBounds().getNorthEast();
+            var sw = rectangle.getBounds().getSouthWest();
+
+            var contentString = &apos;&lt;b&gt;Rectangle moved.&lt;/b&gt;&lt;br&gt;&apos; +
+            &apos;New north-east corner: &apos; + ne.lat() + &apos;, &apos; + ne.lng() + &apos;&lt;br&gt;&apos; +
+            &apos;New south-west corner: &apos; + sw.lat() + &apos;, &apos; + sw.lng();
+
+            infoWindow.setContent(contentString);
+            infoWindow.setPosition(ne);
+            infoWindow.open(map);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
