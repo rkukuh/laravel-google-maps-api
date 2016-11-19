d6b3bd86f3e1fc98ccc1ee9f3ffac8824059bc3a
R. Kukuh <rkukuh@gmail.com>
2016-11-19 14:52:07 +0700

    Remove unused comments

--- resources/views/drawing/circle.blade.php
+++ resources/views/drawing/circle.blade.php
@@ -20,10 +20,6 @@
 
 @push('js')
     <script>
-        // This example creates circles on the map, representing populations in North
-        // America.
-
-        // First, create an object containing LatLng and population for each city.
         var citymap = {
             chicago: {
                 center      : {lat: 41.878, lng: -87.629},
@@ -44,17 +40,13 @@
         };
 
         function initMap() {
-            // Create the map.
             var map = new google.maps.Map(document.getElementById('map'), {
                 zoom        : 4,
                 center      : {lat: 37.090, lng: -95.712},
                 mapTypeId   : 'terrain'
             });
 
-            // Construct the circle for each value in citymap.
-            // Note: We scale the area of the circle based on the population.
             for (var city in citymap) {
-                // Add the circle for this city to the map.
                 var cityCircle = new google.maps.Circle({
                     strokeColor     : '#FF0000',
                     strokeOpacity   : 0.8,
