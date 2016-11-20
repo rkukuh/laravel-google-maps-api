0961dfca3dbaa3f6ba4d5e36630a48ad9330be0e
R. Kukuh <rkukuh@gmail.com>
2016-11-20 16:59:02 +0700

    Remove unused comments

--- resources/views/layer/data-simple.blade.php
+++ resources/views/layer/data-simple.blade.php
@@ -28,7 +28,6 @@
                 center  : {lat: -28, lng: 137}
             });
 
-            // NOTE: This uses cross-domain XHR, and may not work on older browsers.
             map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/google.json');
         }
     </script>
