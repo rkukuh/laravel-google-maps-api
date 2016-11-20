7b1758a0ce3c66d795641d537952b2ff9dac98a6
R. Kukuh <rkukuh@gmail.com>
2016-11-20 18:11:30 +0700

    Spacing

--- resources/views/layer/data-event.blade.php
+++ resources/views/layer/data-event.blade.php
@@ -53,6 +53,7 @@
             });
         }
     </script>
+    
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
 
