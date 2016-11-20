3a2ea404a08a44249719676f1cb50494071e80ad
R. Kukuh <rkukuh@gmail.com>
2016-11-20 18:09:37 +0700

    Add 'show source code' button

--- resources/views/layer/data-event.blade.php
+++ resources/views/layer/data-event.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Data Layer: Event')
 
 @section('content')
-    <h1>Data Layer: Event</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Data Layer: Event
+    </h1>
 
     <div id="map"></div>
     <div id="info-box">?</div>
