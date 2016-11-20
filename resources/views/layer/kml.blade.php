b711bda7dd50c9047c3eecfd09569c211ade9df0
R. Kukuh <rkukuh@gmail.com>
2016-11-20 16:48:45 +0700

    Fill source code tabs

--- resources/views/layer/kml.blade.php
+++ resources/views/layer/kml.blade.php
@@ -35,3 +35,31 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 11,
+                center  : {lat: 41.876, lng: -87.624}
+            });
+
+            var ctaLayer = new google.maps.KmlLayer({
+                url: &apos;http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml&apos;,
+                map: map
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
