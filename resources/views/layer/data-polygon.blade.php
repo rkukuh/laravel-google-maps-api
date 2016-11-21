94c438357b2d861f3b60c5ab208e85a561325cbb
R. Kukuh <rkukuh@gmail.com>
2016-11-21 11:41:43 +0700

    Fill source code tabs

--- resources/views/layer/data-polygon.blade.php
+++ resources/views/layer/data-polygon.blade.php
@@ -62,3 +62,58 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 6,
+                center  : {lat: -33.872, lng: 151.252},
+            });
+
+            // Define the LatLng coordinates for the outer path.
+            var outerCoords = [
+                {lat: -32.364, lng: 153.207}, // north west
+                {lat: -35.364, lng: 153.207}, // south west
+                {lat: -35.364, lng: 158.207}, // south east
+                {lat: -32.364, lng: 158.207}  // north east
+                ];
+
+            // Define the LatLng coordinates for an inner path.
+            var innerCoords1 = [
+                {lat: -33.364, lng: 154.207},
+                {lat: -34.364, lng: 154.207},
+                {lat: -34.364, lng: 155.207},
+                {lat: -33.364, lng: 155.207}
+            ];
+
+            // Define the LatLng coordinates for another inner path.
+            var innerCoords2 = [
+                {lat: -33.364, lng: 156.207},
+                {lat: -34.364, lng: 156.207},
+                {lat: -34.364, lng: 157.207},
+                {lat: -33.364, lng: 157.207}
+            ];
+
+            map.data.add({
+                geometry: new google.maps.Data.Polygon([
+                                outerCoords,
+                                innerCoords1,
+                                innerCoords2
+                            ])
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
