4c715627789c2ed6798d6f64829b97a35986f7b7
R. Kukuh <rkukuh@gmail.com>
2016-11-21 12:11:13 +0700

    Remove unused comments

--- resources/views/layer/heatmap.blade.php
+++ resources/views/layer/heatmap.blade.php
@@ -41,10 +41,6 @@
 
 @push('js')
     <script>
-        // This example requires the Visualization library. Include the libraries=visualization
-        // parameter when you first load the API. For example:
-        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
-
         var map, heatmap;
 
         function initMap() {
