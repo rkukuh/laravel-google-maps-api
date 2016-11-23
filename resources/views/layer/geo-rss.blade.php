dbfd913ea04c1cc2deff67c29b68bdca1fb1044b
R. Kukuh <rkukuh@gmail.com>
2016-11-23 14:29:04 +0700

    Fill all source code tabs

--- resources/views/layer/geo-rss.blade.php
+++ resources/views/layer/geo-rss.blade.php
@@ -36,3 +36,32 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 4,
+                center  : {lat: 49.496675, lng: -102.65625}
+            });
+
+            var georssLayer = new google.maps.KmlLayer({
+                url: &apos;http://api.flickr.com/services/feeds/geo/?g=322338@N20&amp;lang=en-us&amp;format=feed-georss&apos;
+            });
+
+            georssLayer.setMap(map);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
