43cc95f4316babc2e5da57eaf7320523289ae07a
R. Kukuh <rkukuh@gmail.com>
2016-11-23 14:36:17 +0700

    Fill all source code tabs

--- resources/views/layer/bicycling.blade.php
+++ resources/views/layer/bicycling.blade.php
@@ -34,3 +34,30 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 14,
+                center  : {lat: 42.3726399, lng: -71.1096528}
+            });
+
+            var bikeLayer = new google.maps.BicyclingLayer();
+
+            bikeLayer.setMap(map);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
