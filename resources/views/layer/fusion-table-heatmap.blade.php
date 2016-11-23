256d5b9f15914c386a04dc0cf53649f906bc44c4
R. Kukuh <rkukuh@gmail.com>
2016-11-23 13:28:49 +0700

    Fill all source code tabs

--- resources/views/layer/fusion-table-heatmap.blade.php
+++ resources/views/layer/fusion-table-heatmap.blade.php
@@ -42,3 +42,38 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center  : {lat: 10, lng: -140},
+                zoom    : 3
+            });
+
+            var layer = new google.maps.FusionTablesLayer({
+                query: {
+                    select: &apos;location&apos;,
+                    from: &apos;1xWyeuAhIFK_aED1ikkQEGmR8mINSCJO9Vq-BPQ&apos;
+                },
+                heatmap: {
+                    enabled: true
+                }
+            });
+
+            layer.setMap(map);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
