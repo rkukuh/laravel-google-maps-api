5761587c063a2579f93275c4baa3777131856d2b
R. Kukuh <rkukuh@gmail.com>
2016-11-23 13:24:05 +0700

    Add 'show source code' button

--- resources/views/layer/fusion-table-query.blade.php
+++ resources/views/layer/fusion-table-query.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Fusion Table: Query')
 
 @section('content')
-    <h1>Fusion Table: Query</h1>
+    <h1>
+        @include('_shared.button-source-code')
+
+        Fusion Table: Query
+    </h1>
 
     <div id="map"></div>
 @endsection
