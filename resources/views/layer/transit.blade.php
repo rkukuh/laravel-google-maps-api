1a6f2394ed7e07ed9cd92b596ef5dfd3d0101cf7
R. Kukuh <rkukuh@gmail.com>
2016-11-23 14:34:32 +0700

    Fill all source code tabs

--- resources/views/layer/transit.blade.php
+++ resources/views/layer/transit.blade.php
@@ -34,3 +34,30 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 13,
+                center  : {lat: -7.265757, lng: 112.734146}
+            });
+
+            var transitLayer = new google.maps.TransitLayer();
+
+            transitLayer.setMap(map);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
