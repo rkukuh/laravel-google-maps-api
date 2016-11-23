f1818793990056ada6ae08d1915c3200e6a4b050
R. Kukuh <rkukuh@gmail.com>
2016-11-23 14:32:12 +0700

    Center map to Surabaya

--- resources/views/layer/traffic.blade.php
+++ resources/views/layer/traffic.blade.php
@@ -23,7 +23,7 @@
         function initMap() {
             var map = new google.maps.Map(document.getElementById('map'), {
                 zoom    : 13,
-                center  : {lat: 34.04924594193164, lng: -118.24104309082031}
+                center  : {lat: -7.265757, lng: 112.734146}
             });
 
             var trafficLayer = new google.maps.TrafficLayer();
@@ -41,7 +41,7 @@
         function initMap() {
             var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
                 zoom    : 13,
-                center  : {lat: 34.04924594193164, lng: -118.24104309082031}
+                center  : {lat: -7.265757, lng: 112.734146}
             });
 
             var trafficLayer = new google.maps.TrafficLayer();
