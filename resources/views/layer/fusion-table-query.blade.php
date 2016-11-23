96ff0edb485341038f248cd86e5056a27d7e34fd
R. Kukuh <rkukuh@gmail.com>
2016-11-23 13:25:24 +0700

    Spacing

--- resources/views/layer/fusion-table-query.blade.php
+++ resources/views/layer/fusion-table-query.blade.php
@@ -20,22 +20,22 @@
 
 @push('js')
     <script>
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center  : {lat: 41.948766, lng: -87.691497},
-            zoom    : 12
-        });
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center  : {lat: 41.948766, lng: -87.691497},
+                zoom    : 12
+            });
 
-        var layer = new google.maps.FusionTablesLayer({
-            query: {
-                select: 'address',
-                from: '1d7qpn60tAvG4LEg4jvClZbc1ggp8fIGGvpMGzA',
-                where: 'ridership > 5000'
-            }
-        });
+            var layer = new google.maps.FusionTablesLayer({
+                query: {
+                    select: 'address',
+                    from: '1d7qpn60tAvG4LEg4jvClZbc1ggp8fIGGvpMGzA',
+                    where: 'ridership > 5000'
+                }
+            });
 
-        layer.setMap(map);
-    }
+            layer.setMap(map);
+        }
     </script>
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
