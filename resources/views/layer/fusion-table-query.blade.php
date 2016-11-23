b7dd71e028ea81264c7e295795ce2ff4fe8f4ad3
R. Kukuh <rkukuh@gmail.com>
2016-11-23 13:25:06 +0700

    Fill all source code tabs

--- resources/views/layer/fusion-table-query.blade.php
+++ resources/views/layer/fusion-table-query.blade.php
@@ -40,3 +40,36 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center  : {lat: 41.948766, lng: -87.691497},
+                zoom    : 12
+            });
+
+            var layer = new google.maps.FusionTablesLayer({
+                query: {
+                    select: &apos;address&apos;,
+                    from: &apos;1d7qpn60tAvG4LEg4jvClZbc1ggp8fIGGvpMGzA&apos;,
+                    where: &apos;ridership &gt; 5000&apos;
+                }
+            });
+
+            layer.setMap(map);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
