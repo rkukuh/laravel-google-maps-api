6b57fe1e344a51cb2fc250f9cd7975fc3b071e79
R. Kukuh <rkukuh@gmail.com>
2016-11-23 14:31:35 +0700

    Fill all source code tabs

--- resources/views/layer/traffic.blade.php
+++ resources/views/layer/traffic.blade.php
@@ -34,3 +34,30 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 13,
+                center  : {lat: 34.04924594193164, lng: -118.24104309082031}
+            });
+
+            var trafficLayer = new google.maps.TrafficLayer();
+
+            trafficLayer.setMap(map);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
