ecf0a660112fd99c6c4102eebb0f250aeb00d1c6
R. Kukuh <rkukuh@gmail.com>
2016-11-23 13:23:14 +0700

    Fill all source code tabs

--- resources/views/layer/fusion-table-layer.blade.php
+++ resources/views/layer/fusion-table-layer.blade.php
@@ -39,3 +39,35 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center  : {lat: 41.850033, lng: -87.6500523},
+                zoom    : 11
+            });
+
+            var layer = new google.maps.FusionTablesLayer({
+                query: {
+                    select  : &apos;\&apos;Geocodable address\&apos;&apos;,
+                    from    : &apos;1mZ53Z70NsChnBMm-qEYmSDOvLXgrreLTkQUvvg&apos;
+                }
+            });
+
+            layer.setMap(map);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
