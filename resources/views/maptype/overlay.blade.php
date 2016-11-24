0c9aa9a2a37181944e5bc3330fd0908be8758106
R. Kukuh <rkukuh@gmail.com>
2016-11-24 19:09:52 +0700

    Remove unused comments

--- resources/views/maptype/overlay.blade.php
+++ resources/views/maptype/overlay.blade.php
@@ -20,18 +20,6 @@
 
 @push('js')
     <script>
-        /*
-        * This demo illustrates the coordinate system used to display map tiles in the
-        * API.
-        *
-        * Tiles in Google Maps are numbered from the same origin as that for
-        * pixels. For Google's implementation of the Mercator projection, the origin
-        * tile is always at the northwest corner of the map, with x values increasing
-        * from west to east and y values increasing from north to south.
-        *
-        * Try panning and zooming the map to see how the coordinates change.
-        */
-
         /** @constructor */
         function CoordMapType(tileSize) {
             this.tileSize = tileSize;
@@ -57,9 +45,6 @@
                 center  : {lat: -7.265757, lng: 112.734146}
             });
 
-            // Insert this overlay map type as the first overlay map type at
-            // position 0. Note that all overlay map types appear on top of
-            // their parent base map.
             map.overlayMapTypes.insertAt(
                 0,
                 new CoordMapType(new google.maps.Size(256, 256))
