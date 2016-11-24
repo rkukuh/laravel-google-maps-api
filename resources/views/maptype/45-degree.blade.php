421177a4a9ddb16380c51cb5f07f9bd246aaec40
R. Kukuh <rkukuh@gmail.com>
2016-11-24 19:21:10 +0700

    Fill source code tabs

--- resources/views/maptype/45-degree.blade.php
+++ resources/views/maptype/45-degree.blade.php
@@ -33,3 +33,29 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center      : {lat: 36.964, lng: -122.015},
+                zoom        : 18,
+                mapTypeId   : &apos;satellite&apos;
+            });
+
+            map.setTilt(45);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+        src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
+
+@section('source-code-css')
+    #map { height: 500px; }
+@endsection
+
+@section('source-code-html')
+    <div id="map"></div>
+@endsection
