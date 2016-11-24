e8e27597d77b9f177be3ef02729a8d7efeccb923
R. Kukuh <rkukuh@gmail.com>
2016-11-24 19:12:01 +0700

    Remove unused comments

--- resources/views/maptype/image.blade.php
+++ resources/views/maptype/image.blade.php
@@ -54,22 +54,16 @@
             map.setMapTypeId('moon');
         }
 
-        // Normalizes the coords that tiles repeat across the x axis (horizontally)
-        // like the standard Google map tiles.
         function getNormalizedCoord(coord, zoom) {
             var y = coord.y;
             var x = coord.x;
 
-            // tile range in one direction range is dependent on zoom level
-            // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
             var tileRange = 1 << zoom;
 
-            // don't repeat across y-axis (vertically)
             if (y < 0 || y >= tileRange) {
                 return null;
             }
 
-            // repeat across x-axis
             if (x < 0 || x >= tileRange) {
                 x = (x % tileRange + tileRange) % tileRange;
             }
