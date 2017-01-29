7fa31f4211c067ee29eca957ac9828b05102cc36
R. Kukuh <rkukuh@gmail.com>
2017-01-29 17:45:03 +0700

    Set center map to Surabaya

--- resources/views/service/geocoding-reverse.blade.php
+++ resources/views/service/geocoding-reverse.blade.php
@@ -43,8 +43,8 @@
     <script>
         function initMap() {
             var map = new google.maps.Map(document.getElementById('map'), {
-                zoom    : 8,
-                center  : {lat: 40.731, lng: -73.997}
+                zoom    : 10,
+                center: {lat: -7.265757, lng: 112.734146},
             });
 
             var geocoder   = new google.maps.Geocoder;
