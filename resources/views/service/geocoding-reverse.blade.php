7a8406cfd8cb79e05b4212b3e497bb9ead67893a
R. Kukuh <rkukuh@gmail.com>
2017-01-29 17:50:07 +0700

    Fill CSS tab

--- resources/views/service/geocoding-reverse.blade.php
+++ resources/views/service/geocoding-reverse.blade.php
@@ -141,7 +141,28 @@
 @endsection
 
 @section('source-code-css')
+
     #map { height: 500px; }
+
+    #floating-panel {
+        position: absolute;
+        top: 10px;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+        left: 55%;
+        margin-left: -180px;
+        width: 350px;
+    }
+
+    #latlng {
+        width: 225px;
+    }
 @endsection
 
 @section('source-code-html')
