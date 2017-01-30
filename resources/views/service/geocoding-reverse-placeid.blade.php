3b4e1a2ce98f2fd27941f40fe0415e12443de74d
R. Kukuh <rkukuh@gmail.com>
2017-01-30 18:08:07 +0700

    Fill HTML tab

--- resources/views/service/geocoding-reverse-placeid.blade.php
+++ resources/views/service/geocoding-reverse-placeid.blade.php
@@ -165,5 +165,13 @@
 @endsection
 
 @section('source-code-html')
-    <div id="map"></div>
+
+    &lt;div id=&quot;floating-panel&quot;&gt;
+        &lt;!-- Supply a default place ID for a place in Brooklyn, New York. --&gt;
+        &lt;input id=&quot;place-id&quot; type=&quot;text&quot; value=&quot;ChIJd8BlQ2BZwokRAFUEcm_qrcA&quot;&gt;
+        &lt;input id=&quot;submit&quot; type=&quot;button&quot; value=&quot;Find&quot;&gt;
+    &lt;/div&gt;
+
+    &lt;span class=&quot;text-primary&quot;&gt;Use &quot;Server Key&quot; instead of &quot;Browser Key&quot;&lt;/span&gt;
+    &lt;div id=&quot;map&quot;&gt;&lt;/div&gt;
 @endsection
