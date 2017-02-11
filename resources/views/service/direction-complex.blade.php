ad1e4e73142db210106bb48890ddb66528a5a992
R. Kukuh <rkukuh@gmail.com>
2017-02-11 12:10:46 +0700

    Remove unused comments

--- resources/views/service/direction-complex.blade.php
+++ resources/views/service/direction-complex.blade.php
@@ -66,22 +66,17 @@
         function initMap() {
             var markerArray = [];
 
-            // Instantiate a directions service.
             var directionsService = new google.maps.DirectionsService;
 
-            // Create a map and center it on Manhattan.
             var map = new google.maps.Map(document.getElementById('map'), {
                 zoom    : 13,
                 center  : {lat: 40.771, lng: -73.974}
             });
 
-            // Create a renderer for directions and bind it to the map.
             var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
 
-            // Instantiate an info window to hold step text.
             var stepDisplay = new google.maps.InfoWindow;
 
-            // Display the route between the initial start and end selections.
             calculateAndDisplayRoute(
                 directionsDisplay,
                 directionsService,
@@ -90,7 +85,6 @@
                 map
             );
 
-            // Listen to change events from the start and end lists.
             var onChangeHandler = function() {
                 calculateAndDisplayRoute(
                     directionsDisplay,
@@ -107,21 +101,16 @@
 
         function calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map)
         {
-            // First, remove any existing markers from the map.
             for (var i = 0; i < markerArray.length; i++) {
                 markerArray[i].setMap(null);
             }
 
-            // Retrieve the start and end locations and create a DirectionsRequest using
-            // WALKING directions.
             directionsService.route({
                 origin      : document.getElementById('start').value,
                 destination : document.getElementById('end').value,
                 travelMode  : 'WALKING'
             },
             function(response, status) {
-                // Route the directions and pass the response to a function to create
-                // markers for each step.
                 if (status === 'OK') {
                     document.getElementById('warnings-panel').innerHTML = '<b>' + response.routes[0].warnings + '</b>';
 
@@ -136,9 +125,6 @@
         }
 
         function showSteps(directionResult, markerArray, stepDisplay, map) {
-            // For each step, place a marker, and add the text to the marker's infowindow.
-            // Also attach the marker to an array so we can keep track of it and remove it
-            // when calculating new routes.
             var myRoute = directionResult.routes[0].legs[0];
 
             for (var i = 0; i < myRoute.steps.length; i++) {
@@ -157,8 +143,6 @@
 
         function attachInstructionText(stepDisplay, marker, text, map) {
             google.maps.event.addListener(marker, 'click', function() {
-                // Open an info window when the marker is clicked on, containing the text
-                // of the step.
                 stepDisplay.setContent(text);
                 stepDisplay.open(map, marker);
             });
