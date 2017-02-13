9eec052a1eea2192cf5312118a808006587168db
R. Kukuh <rkukuh@gmail.com>
2017-02-13 14:04:58 +0700

    Remove unused comment

--- resources/views/service/elevation-path.blade.php
+++ resources/views/service/elevation-path.blade.php
@@ -23,20 +23,17 @@
     <script src="https://www.google.com/jsapi"></script>
 
     <script>
-        // Load the Visualization API and the columnchart package.
         google.load('visualization', '1', {packages: ['columnchart']});
 
         function initMap() {
-            // The following path marks a path from Mt. Whitney, the highest point in the
-            // continental United States to Badwater, Death Valley, the lowest point.
             var path = [
-                {lat: 36.579, lng: -118.292},  // Mt. Whitney
-                {lat: 36.606, lng: -118.0638}, // Lone Pine
-                {lat: 36.433, lng: -117.951},  // Owens Lake
-                {lat: 36.588, lng: -116.943},  // Beatty Junction
-                {lat: 36.34, lng: -117.468},   // Panama Mint Springs
+                {lat: 36.579, lng: -118.292},
+                {lat: 36.606, lng: -118.0638},
+                {lat: 36.433, lng: -117.951},
+                {lat: 36.588, lng: -116.943},
+                {lat: 36.34, lng: -117.468},
                 {lat: 36.24, lng: -116.832}
-            ];  // Badwater, Death Valley
+            ];
 
             var map = new google.maps.Map(document.getElementById('map'), {
                 zoom        : 8,
@@ -44,15 +41,12 @@
                 mapTypeId   : 'terrain'
             });
 
-            // Create an ElevationService.
             var elevator = new google.maps.ElevationService;
 
-            // Draw the path, using the Visualization API and the Elevation service.
             displayPathElevation(path, elevator, map);
         }
 
         function displayPathElevation(path, elevator, map) {
-            // Display a polyline of the elevation path.
             new google.maps.Polyline({
                 path        : path,
                 strokeColor : '#0000CC',
@@ -60,33 +54,23 @@
                 map         : map
             });
 
-            // Create a PathElevationRequest object using this array.
-            // Ask for 256 samples along that path.
-            // Initiate the path request.
             elevator.getElevationAlongPath({
                 'path'      : path,
                 'samples'   : 256
             }, plotElevation);
         }
 
-        // Takes an array of ElevationResult objects, draws the path on the map
-        // and plots the elevation profile on a Visualization API ColumnChart.
         function plotElevation(elevations, status) {
             var chartDiv = document.getElementById('elevation_chart');
 
             if (status !== 'OK') {
-                // Show the error code inside the chartDiv.
                 chartDiv.innerHTML = 'Cannot show elevation: request failed because ' + status;
+
                 return;
             }
 
-            // Create a new chart in the elevation_chart DIV.
             var chart = new google.visualization.ColumnChart(chartDiv);
 
-            // Extract the data from which to populate the chart.
-            // Because the samples are equidistant, the 'Sample'
-            // column here does double duty as distance along the
-            // X axis.
             var data = new google.visualization.DataTable();
 
             data.addColumn('string', 'Sample');
@@ -96,7 +80,6 @@
                 data.addRow(['', elevations[i].elevation]);
             }
 
-            // Draw the chart using the data within its DIV.
             chart.draw(data, {
                 height: 150,
                 legend: 'none',
