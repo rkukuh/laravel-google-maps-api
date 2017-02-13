1ce2706cf9636b179795812b64b5ae6a4677232a
R. Kukuh <rkukuh@gmail.com>
2017-02-13 13:32:06 +0700

    Fill javascript tab

--- resources/views/service/direction-draggable.blade.php
+++ resources/views/service/direction-draggable.blade.php
@@ -122,8 +122,68 @@
 @section('source-code-javascript')
 
     &lt;script&gt;
-        //
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom    : 4,
+                center  : {lat: -24.345, lng: 134.46}  // Australia.
+            });
+
+            var directionsService = new google.maps.DirectionsService;
+
+            var directionsDisplay = new google.maps.DirectionsRenderer({
+                draggable   : true,
+                map         : map,
+                panel       : document.getElementById(&apos;right-panel&apos;)
+            });
+
+            directionsDisplay.addListener(&apos;directions_changed&apos;, function() {
+                computeTotalDistance(directionsDisplay.getDirections());
+            });
+
+            displayRoute(
+                &apos;Perth, WA&apos;,
+                &apos;Sydney, NSW&apos;,
+                directionsService,
+                directionsDisplay
+            );
+        }
+
+        function displayRoute(origin, destination, service, display) {
+            service.route({
+                origin      : origin,
+                destination : destination,
+                waypoints   : [
+                    {location: &apos;Adelaide, SA&apos;},
+                    {location: &apos;Broken Hill, NSW&apos;}
+                ],
+                travelMode  : &apos;DRIVING&apos;,
+                avoidTolls  : true
+            },
+            function(response, status) {
+                if (status === &apos;OK&apos;) {
+                    display.setDirections(response);
+                }
+                else {
+                    alert(&apos;Could not display directions due to: &apos; + status);
+                }
+            });
+        }
+
+        function computeTotalDistance(result) {
+            var total   = 0;
+            var myroute = result.routes[0];
+
+            for (var i = 0; i &lt; myroute.legs.length; i++) {
+                total += myroute.legs[i].distance.value;
+            }
+
+            total = total / 1000;
+
+            document.getElementById(&apos;total&apos;).innerHTML = total + &apos; km&apos;;
+        }
     &lt;/script&gt;
+
+    &lt;script async defer src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $server_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
 @endsection
 
 @section('source-code-css')
