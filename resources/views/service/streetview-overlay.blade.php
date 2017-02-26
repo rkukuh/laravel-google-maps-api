fb43bfcec52ccf19c2cb892d6762e9b8fbc9e40c
R. Kukuh <rkukuh@gmail.com>
2017-02-26 17:11:08 +0700

    Fill javascript tab

--- resources/views/service/streetview-overlay.blade.php
+++ resources/views/service/streetview-overlay.blade.php
@@ -100,8 +100,63 @@
 @section('source-code-javascript')
 
     &lt;script&gt;
-        //
+        var panorama;
+
+        function initMap() {
+            var astorPlace = {lat: 40.729884, lng: -73.990988};
+
+            // Set up the map
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center  : astorPlace,
+                zoom    : 18,
+                streetViewControl: false
+            });
+
+            // Set up the markers on the map
+            var cafeMarker = new google.maps.Marker({
+                position    : {lat: 40.730031, lng: -73.991428},
+                map         : map,
+                icon        : &apos;http://chart.apis.google.com/chart?chst=d_map_pin_icon&amp;chld=cafe|FFFF00&apos;,
+                title       : &apos;Cafe&apos;
+            });
+
+            var bankMarker = new google.maps.Marker({
+                position    : {lat: 40.729681, lng: -73.991138},
+                map         : map,
+                icon        : &apos;http://chart.apis.google.com/chart?chst=d_map_pin_icon&amp;chld=dollar|FFFF00&apos;,
+                title       : &apos;Bank&apos;
+            });
+
+            var busMarker = new google.maps.Marker({
+                position    : {lat: 40.729559, lng: -73.990741},
+                map         : map,
+                icon        : &apos;http://chart.apis.google.com/chart?chst=d_map_pin_icon&amp;chld=bus|FFFF00&apos;,
+                title       : &apos;Bus Stop&apos;
+            });
+
+            // We get the map&apos;s default panorama and set up some defaults.
+            // Note that we don&apos;t yet set it visible.
+            panorama = map.getStreetView();
+
+            panorama.setPosition(astorPlace);
+            panorama.setPov(/** @type {google.maps.StreetViewPov} */({
+                heading : 265,
+                pitch   : 0
+            }));
+        }
+
+        function toggleStreetView() {
+            var toggle = panorama.getVisible();
+
+            if (toggle == false) {
+                panorama.setVisible(true);
+            } else {
+                panorama.setVisible(false);
+            }
+        }
     &lt;/script&gt;
+
+    &lt;script async defer src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
 @endsection
 
 @section('source-code-css')
