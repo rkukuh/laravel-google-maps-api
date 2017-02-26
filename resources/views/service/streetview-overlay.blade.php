41f01deb7b4d68772fb420971bb495a66105f7ab
R. Kukuh <rkukuh@gmail.com>
2017-02-26 17:11:41 +0700

    Remove unused comment

--- resources/views/service/streetview-overlay.blade.php
+++ resources/views/service/streetview-overlay.blade.php
@@ -43,14 +43,12 @@
         function initMap() {
             var astorPlace = {lat: 40.729884, lng: -73.990988};
 
-            // Set up the map
             var map = new google.maps.Map(document.getElementById('map'), {
                 center  : astorPlace,
                 zoom    : 18,
                 streetViewControl: false
             });
 
-            // Set up the markers on the map
             var cafeMarker = new google.maps.Marker({
                 position    : {lat: 40.730031, lng: -73.991428},
                 map         : map,
@@ -72,8 +70,6 @@
                 title       : 'Bus Stop'
             });
 
-            // We get the map's default panorama and set up some defaults.
-            // Note that we don't yet set it visible.
             panorama = map.getStreetView();
 
             panorama.setPosition(astorPlace);
