63b4785adfa20205fe732fe67aa2a94abab57289
R. Kukuh <rkukuh@gmail.com>
2017-02-27 17:32:24 +0700

    Remove unused comment

--- resources/views/service/streetview-control.blade.php
+++ resources/views/service/streetview-control.blade.php
@@ -21,8 +21,6 @@
 @push('js')
     <script>
         function initPano() {
-            // Note: constructed panorama objects have visible: true
-            // set by default.
             var panorama = new google.maps.StreetViewPanorama(
                 document.getElementById('map'), {
                     position: {lat: 42.345573, lng: -71.098326},
