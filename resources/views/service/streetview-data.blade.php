daa225ecfae7ae8e138bc537fd76a4fa93ec4523
R. Kukuh <rkukuh@gmail.com>
2017-02-28 17:40:30 +0700

    Remove unused comment

--- resources/views/service/streetview-data.blade.php
+++ resources/views/service/streetview-data.blade.php
@@ -34,19 +34,14 @@
 
             panorama     = new google.maps.StreetViewPanorama(document.getElementById('pano'));
 
-            // Set up the map.
             map = new google.maps.Map(document.getElementById('map'), {
                 center: surabaya,
                 zoom: 16,
                 streetViewControl: false
             });
 
-            // Set the initial Street View camera to the center of the map
             sv.getPanorama({location: surabaya, radius: 50}, processSVData);
 
-            // Look for a nearby Street View panorama when the map is clicked.
-            // getPanoramaByLocation will return the nearest pano when the
-            // given radius is 50 meters or less.
             map.addListener('click', function(event) {
                 sv.getPanorama({location: event.latLng, radius: 50}, processSVData);
             });
@@ -72,7 +67,6 @@
                 marker.addListener('click', function() {
                     var markerPanoID = data.location.pano;
 
-                    // Set the Pano to use the passed panoID.
                     panorama.setPano(markerPanoID);
                     panorama.setPov({
                         heading: 270,
