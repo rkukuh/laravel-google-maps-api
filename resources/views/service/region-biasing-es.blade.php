c6d9dc241ec016837ea55dd39899b231f916ab13
R. Kukuh <rkukuh@gmail.com>
2017-03-05 20:44:34 +0700

    Spacing

--- resources/views/service/region-biasing-es.blade.php
+++ resources/views/service/region-biasing-es.blade.php
@@ -20,24 +20,24 @@
 
 @push('js')
     <script>
-    function initMap() {
-        var map      = new google.maps.Map(document.getElementById('map'), {zoom: 8});
-        var geocoder = new google.maps.Geocoder;
+        function initMap() {
+            var map      = new google.maps.Map(document.getElementById('map'), {zoom: 8});
+            var geocoder = new google.maps.Geocoder;
 
-        geocoder.geocode({'address': 'Toledo'}, function(results, status) {
-            if (status === 'OK') {
-                map.setCenter(results[0].geometry.location);
+            geocoder.geocode({'address': 'Toledo'}, function(results, status) {
+                if (status === 'OK') {
+                    map.setCenter(results[0].geometry.location);
 
-                new google.maps.Marker({
-                    map     : map,
-                    position: results[0].geometry.location
-                });
-            }
-            else {
-                window.alert('Geocode was not successful for the following reason: ' + status);
-            }
-        });
-    }
+                    new google.maps.Marker({
+                        map     : map,
+                        position: results[0].geometry.location
+                    });
+                }
+                else {
+                    window.alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+        }
     </script>
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $server_key }}&region=ES&callback=initMap"></script>
