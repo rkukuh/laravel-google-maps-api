83485a1eed959738a4658f568a655d43f9220848
R. Kukuh <rkukuh@gmail.com>
2017-03-07 21:04:07 +0700

    Fill javascript tab

--- resources/views/service/maximum-zoom.blade.php
+++ resources/views/service/maximum-zoom.blade.php
@@ -57,8 +57,38 @@
 @section('source-code-javascript')
 
     &lt;script&gt;
-        //
+        var map;
+        var maxZoomService;
+        var infoWindow;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom: 11,
+                center: {lat: 35.6894, lng: 139.692},
+                mapTypeId: &apos;hybrid&apos;
+            });
+
+            infoWindow      = new google.maps.InfoWindow();
+            maxZoomService  = new google.maps.MaxZoomService();
+
+            map.addListener(&apos;click&apos;, showMaxZoom);
+        }
+
+        function showMaxZoom(e) {
+            maxZoomService.getMaxZoomAtLatLng(e.latLng, function(response) {
+                if (response.status !== &apos;OK&apos;) {
+                    infoWindow.setContent(&apos;Error in MaxZoomService&apos;);
+                } else {
+                    infoWindow.setContent(&apos;The maximum zoom at this location is: &apos; + response.zoom);
+                }
+
+                infoWindow.setPosition(e.latLng);
+                infoWindow.open(map);
+            });
+        }
     &lt;/script&gt;
+
+    &lt;script async defer src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;callback=initMap&quot;&gt;&lt;/script&gt;
 @endsection
 
 @section('source-code-css')
