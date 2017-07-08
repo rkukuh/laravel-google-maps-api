c320454d0ae16784cd14ad178b87a0baf5716e3b
R. Kukuh <rkukuh@gmail.com>
2017-07-08 16:09:17 +0700

    Remove unused comments

--- resources/views/library/geometry-encoding.blade.php
+++ resources/views/library/geometry-encoding.blade.php
@@ -54,10 +54,6 @@
 
 @push('js')
     <script>
-        // This example requires the Geometry library. Include the libraries=geometry
-        // parameter when you first load the API. For example:
-        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=geometry">
-
         function initMap() {
             var map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 14,
@@ -71,23 +67,16 @@
                 map: map
             });
 
-            // Add a listener for the click event
             google.maps.event.addListener(map, 'click', function(event) {
                 addLatLngToPoly(event.latLng, poly);
             });
         }
 
-        /**
-        * Handles click events on a map, and adds a new point to the Polyline.
-        * Updates the encoding text area with the path's encoded values.
-        */
         function addLatLngToPoly(latLng, poly) {
             var path = poly.getPath();
-            // Because path is an MVCArray, we can simply append a new coordinate
-            // and it will automatically appear
+
             path.push(latLng);
 
-            // Update the text field to display the polyline encodings
             var encodeString = google.maps.geometry.encoding.encodePath(path);
 
             if (encodeString) {
