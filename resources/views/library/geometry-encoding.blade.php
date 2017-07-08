40d8ad3bc8f0365272a9b2ca6ac6380764a45c27
R. Kukuh <rkukuh@gmail.com>
2017-07-08 16:07:03 +0700

    Fill js source code section

--- resources/views/library/geometry-encoding.blade.php
+++ resources/views/library/geometry-encoding.blade.php
@@ -99,3 +99,52 @@
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&libraries=geometry&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        // This example requires the Geometry library. Include the libraries=geometry
+        // parameter when you first load the API. For example:
+        // &lt;script src=&quot;https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&amp;libraries=geometry&quot;&gt;
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                zoom: 14,
+                center: {lat: 34.366, lng: -89.519}
+            });
+
+            var poly = new google.maps.Polyline({
+                strokeColor: &apos;#000000&apos;,
+                strokeOpacity: 1,
+                strokeWeight: 3,
+                map: map
+            });
+
+            // Add a listener for the click event
+            google.maps.event.addListener(map, &apos;click&apos;, function(event) {
+                addLatLngToPoly(event.latLng, poly);
+            });
+        }
+
+        /**
+        * Handles click events on a map, and adds a new point to the Polyline.
+        * Updates the encoding text area with the path&apos;s encoded values.
+        */
+        function addLatLngToPoly(latLng, poly) {
+            var path = poly.getPath();
+            // Because path is an MVCArray, we can simply append a new coordinate
+            // and it will automatically appear
+            path.push(latLng);
+
+            // Update the text field to display the polyline encodings
+            var encodeString = google.maps.geometry.encoding.encodePath(path);
+
+            if (encodeString) {
+                document.getElementById(&apos;encoded-polyline&apos;).value = encodeString;
+            }
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer
+    src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;libraries=geometry&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
