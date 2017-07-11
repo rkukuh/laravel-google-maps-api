4f3c9b739fb5b18a777fc13f0ad655f3303f1d53
R. Kukuh <rkukuh@gmail.com>
2017-07-11 17:39:08 +0700

    Remove unused comments

--- resources/views/library/places-search-pagination.blade.php
+++ resources/views/library/places-search-pagination.blade.php
@@ -80,10 +80,6 @@
 
 @push('js')
     <script>
-        // This example requires the Places library. Include the libraries=places
-        // parameter when you first load the API. For example:
-        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
         var map;
 
         function initMap() {
