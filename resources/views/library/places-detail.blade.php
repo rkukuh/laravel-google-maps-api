291498e8031e4832775ae0c04f8177bd8f471c96
R. Kukuh <rkukuh@gmail.com>
2017-07-11 17:32:31 +0700

    Fill js source code section

--- resources/views/library/places-detail.blade.php
+++ resources/views/library/places-detail.blade.php
@@ -57,3 +57,44 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&libraries=places&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        // This example requires the Places library. Include the libraries=places
+        // parameter when you first load the API. For example:
+        // &lt;script src=&quot;https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&amp;libraries=places&quot;&gt;
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center: {lat: -33.866, lng: 151.196},
+                zoom: 15
+            });
+
+            var infowindow  = new google.maps.InfoWindow();
+            var service     = new google.maps.places.PlacesService(map);
+
+            service.getDetails({
+                placeId: &apos;ChIJN1t_tDeuEmsRUsoyG83frY4&apos;
+            },
+            function(place, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: place.geometry.location
+                    });
+
+                    google.maps.event.addListener(marker, &apos;click&apos;, function() {
+                        infowindow.setContent(&apos;&lt;div&gt;&lt;strong&gt;&apos; + place.name + &apos;&lt;/strong&gt;&lt;br&gt;&apos; +
+                                              &apos;Place ID: &apos; + place.place_id + &apos;&lt;br&gt;&apos; +
+                                              place.formatted_address + &apos;&lt;/div&gt;&apos;);
+
+                        infowindow.open(map, this);
+                    });
+                }
+            });
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;libraries=places&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
