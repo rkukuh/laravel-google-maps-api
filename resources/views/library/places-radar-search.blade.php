297404e85afa7720d31a074cfb765add171e556a
R. Kukuh <rkukuh@gmail.com>
2017-07-14 19:27:05 +0700

    Remove unused comments

--- resources/views/library/places-radar-search.blade.php
+++ resources/views/library/places-radar-search.blade.php
@@ -20,10 +20,6 @@
 
 @push('js')
     <script>
-        // This example requires the Places library. Include the libraries=places
-        // parameter when you first load the API. For example:
-        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
         var map;
         var infoWindow;
         var service;
