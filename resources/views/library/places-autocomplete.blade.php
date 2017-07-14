ca09fd63b908b8ca97499a14dd68e52d5daee9bb
R. Kukuh <rkukuh@gmail.com>
2017-07-14 19:26:38 +0700

    Fill js source code section

--- resources/views/library/places-autocomplete.blade.php
+++ resources/views/library/places-autocomplete.blade.php
@@ -171,3 +171,100 @@
 
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $browser_key }}&libraries=places&callback=initMap"></script>
 @endpush
+
+@section('source-code-javascript')
+
+    &lt;script&gt;
+        // This example requires the Places library. Include the libraries=places
+        // parameter when you first load the API. For example:
+        // &lt;script src=&quot;https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&amp;libraries=places&quot;&gt;
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById(&apos;map&apos;), {
+                center: {lat: -7.265757, lng: 112.734146},
+                zoom: 13
+            });
+
+            var input = /** @type {!HTMLInputElement} */(
+                document.getElementById(&apos;pac-input&apos;)
+            );
+
+            var types = document.getElementById(&apos;type-selector&apos;);
+
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+            var autocomplete = new google.maps.places.Autocomplete(input);
+
+            autocomplete.bindTo(&apos;bounds&apos;, map);
+
+            var infowindow = new google.maps.InfoWindow();
+
+            var marker = new google.maps.Marker({
+                map: map,
+                anchorPoint: new google.maps.Point(0, -29)
+            });
+
+            autocomplete.addListener(&apos;place_changed&apos;, function() {
+                infowindow.close();
+                marker.setVisible(false);
+
+                var place = autocomplete.getPlace();
+
+                if (!place.geometry) {
+                    window.alert(&quot;Autocomplete&apos;s returned place contains no geometry&quot;);
+                    return;
+                }
+
+                // If the place has a geometry, then present it on a map.
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                } else {
+                    map.setCenter(place.geometry.location);
+                    map.setZoom(17);  // Why 17? Because it looks good.
+                }
+
+                marker.setIcon(/** @type {google.maps.Icon} */({
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(35, 35)
+                }));
+
+                marker.setPosition(place.geometry.location);
+                marker.setVisible(true);
+
+                var address = &apos;&apos;;
+
+                if (place.address_components) {
+                    address = [
+                        (place.address_components[0] &amp;&amp; place.address_components[0].short_name || &apos;&apos;),
+                        (place.address_components[1] &amp;&amp; place.address_components[1].short_name || &apos;&apos;),
+                        (place.address_components[2] &amp;&amp; place.address_components[2].short_name || &apos;&apos;)
+                    ].join(&apos; &apos;);
+                }
+
+                infowindow.setContent(&apos;&lt;div&gt;&lt;strong&gt;&apos; + place.name + &apos;&lt;/strong&gt;&lt;br&gt;&apos; + address);
+                infowindow.open(map, marker);
+            });
+
+            // Sets a listener on a radio button to change the filter type on Places
+            // Autocomplete.
+            function setupClickListener(id, types) {
+                var radioButton = document.getElementById(id);
+
+                radioButton.addEventListener(&apos;click&apos;, function() {
+                    autocomplete.setTypes(types);
+                });
+            }
+
+            setupClickListener(&apos;changetype-all&apos;, []);
+            setupClickListener(&apos;changetype-address&apos;, [&apos;address&apos;]);
+            setupClickListener(&apos;changetype-establishment&apos;, [&apos;establishment&apos;]);
+            setupClickListener(&apos;changetype-geocode&apos;, [&apos;geocode&apos;]);
+        }
+    &lt;/script&gt;
+
+    &lt;script async defer src=&quot;https://maps.googleapis.com/maps/api/js?key={{ $browser_key_placeholder }}&amp;libraries=places&amp;callback=initMap&quot;&gt;&lt;/script&gt;
+@endsection
